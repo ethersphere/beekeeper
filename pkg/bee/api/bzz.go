404bc466b1dbe51e14df959d3813c65798256869
svetomir <svetomirsmiljkovic@gmail.com>
2020-05-04 16:28:54 +0200

    Small refactor

--- pkg/bee/api/bzz.go
+++ pkg/bee/api/bzz.go
@@ -9,18 +9,6 @@ import (
 // BzzService ...
 type BzzService service
 
-// BzzGetResponse ...
-type BzzGetResponse struct {
-	Hash string `json:"hash"`
-}
-
-// Get ...
-func (b *BzzService) Get(ctx context.Context, addr string) (resp BzzGetResponse, err error) {
-	var r BzzGetResponse
-	err = b.client.request(ctx, http.MethodPost, "/bzz/"+addr, nil, &r)
-	return r, err
-}
-
 // BzzUploadResponse ...
 type BzzUploadResponse struct {
 	Hash string `json:"hash"`
