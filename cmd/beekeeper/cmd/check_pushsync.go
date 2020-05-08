d19aa5561a8de1044c0f8d3f1a9975be89b4e7b7
svetomir <svetomirsmiljkovic@gmail.com>
2020-05-08 16:15:07 +0200

    Add flags for upload sizing

--- cmd/beekeeper/cmd/check_pushsync.go
+++ cmd/beekeeper/cmd/check_pushsync.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/ethersphere/beekeeper/pkg/check"
 
 	"github.com/spf13/cobra"
@@ -8,8 +10,10 @@ import (
 
 func (c *command) initCheckPushSync() *cobra.Command {
 	const (
-		optionNameSeed       = "seed"
-		optionNameRandomSeed = "random-seed"
+		optionNameChunksPerNode   = "chunks-per-node"
+		optionNameRandomSeed      = "random-seed"
+		optionNameSeed            = "seed"
+		optionNameUploadNodeCount = "upload-node-count"
 	)
 
 	cmd := &cobra.Command{
@@ -17,23 +21,30 @@ func (c *command) initCheckPushSync() *cobra.Command {
 		Short: "Checks push sync",
 		Long:  `Checks push sync`,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			if c.config.GetInt(optionNameUploadNodeCount) > c.config.GetInt(optionNameNodeCount) {
+				return errors.New("upload-node-count must be less or equal to node-count")
+			}
 			return check.PushSync(check.PushSyncOptions{
 				APIHostnamePattern:      c.config.GetString(optionNameAPIHostnamePattern),
 				APIDomain:               c.config.GetString(optionNameAPIDomain),
+				ChunksPerNode:           c.config.GetInt(optionNameChunksPerNode),
 				DebugAPIHostnamePattern: c.config.GetString(optionNameDebugAPIHostnamePattern),
 				DebugAPIDomain:          c.config.GetString(optionNameDebugAPIDomain),
 				DisableNamespace:        disableNamespace,
 				Namespace:               c.config.GetString(optionNameNamespace),
 				NodeCount:               c.config.GetInt(optionNameNodeCount),
-				Seed:                    c.config.GetInt64(optionNameSeed),
 				RandomSeed:              c.config.GetBool(optionNameRandomSeed),
+				Seed:                    c.config.GetInt64(optionNameSeed),
+				UploadNodeCount:         c.config.GetInt(optionNameUploadNodeCount),
 			})
 		},
 		PreRunE: c.checkPreRunE,
 	}
 
-	cmd.Flags().Int64P(optionNameSeed, "s", 1, "seed")
+	cmd.Flags().IntP(optionNameChunksPerNode, "p", 1, "number of chunks to upload per node")
 	cmd.Flags().BoolP(optionNameRandomSeed, "r", true, "random seed")
+	cmd.Flags().Int64P(optionNameSeed, "s", 1, "seed")
+	cmd.Flags().IntP(optionNameUploadNodeCount, "u", 1, "number of nodes to upload chunks to")
 
 	return cmd
 }
--- pkg/check/pushsync.go
+++ pkg/check/pushsync.go
@@ -14,6 +14,7 @@ import (
 type PushSyncOptions struct {
 	APIHostnamePattern      string
 	APIDomain               string
+	ChunksPerNode           int
 	DebugAPIHostnamePattern string
 	DebugAPIDomain          string
 	DisableNamespace        bool
@@ -21,6 +22,8 @@ type PushSyncOptions struct {
 	NodeCount               int
 	RandomSeed              bool
 	Seed                    int64
+	TargetNode              int
+	UploadNodeCount         int
 }
 
 // PushSync ...
@@ -36,45 +39,53 @@ func PushSync(opts PushSyncOptions) (err error) {
 	rand.Seed(seed)
 	fmt.Printf("seed: %d\n", seed)
 
-	for i := 0; i < opts.NodeCount; i++ {
-		chunkSize := rand.Intn(maxChunkSize)
-		fmt.Printf("chunkSize: %d\n", chunkSize)
+	for i := 0; i < opts.UploadNodeCount; i++ {
+		fmt.Printf("Node %d:\n", i)
+		for j := 0; j < opts.ChunksPerNode; j++ {
+			chunkSize := rand.Intn(maxChunkSize)
+			fmt.Printf("Chunk %d size: %d\n", j, chunkSize)
 
-		chunk := make([]byte, chunkSize)
-		if _, err := rand.Read(chunk); err != nil {
-			return err
+			chunk := make([]byte, chunkSize)
+			if _, err := rand.Read(chunk); err != nil {
+				return err
+			}
+
+			APIURL, err := createURL(scheme, opts.APIHostnamePattern, opts.Namespace, opts.APIDomain, i, opts.DisableNamespace)
+			if err != nil {
+				return err
+			}
+
+			c := api.NewClient(APIURL, nil)
+			ctx := context.Background()
+			data := bytes.NewReader(chunk)
+
+			r, err := c.Bzz.Upload(ctx, data)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Chunk %d hash: %s\n", j, r.Hash)
+
+			debugAPIURL, err := createURL(scheme, opts.DebugAPIHostnamePattern, opts.Namespace, opts.DebugAPIDomain, i, opts.DisableNamespace)
+			if err != nil {
+				return err
+			}
+
+			dc := debugapi.NewClient(debugAPIURL, nil)
+			ctx = context.Background()
+
+			resp, err := dc.Node.HasChunk(ctx, r.Hash)
+			if err != nil {
+				return err
+			}
+
+			if resp.Message == "OK" {
+				fmt.Printf("Chunk %d found\n", j)
+			} else {
+				fmt.Printf("Chunk %d not found\n", j)
+			}
+
 		}
-
-		APIURL, err := createURL(scheme, opts.APIHostnamePattern, opts.Namespace, opts.APIDomain, i, opts.DisableNamespace)
-		if err != nil {
-			return err
-		}
-
-		c := api.NewClient(APIURL, nil)
-		ctx := context.Background()
-		data := bytes.NewReader(chunk)
-
-		r, err := c.Bzz.Upload(ctx, data)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Node %d. Hash: %s\n", i, r.Hash)
-
-		debugAPIURL, err := createURL(scheme, opts.DebugAPIHostnamePattern, opts.Namespace, opts.DebugAPIDomain, i, opts.DisableNamespace)
-		if err != nil {
-			return err
-		}
-
-		dc := debugapi.NewClient(debugAPIURL, nil)
-		ctx = context.Background()
-
-		resp, err := dc.Node.HasChunk(ctx, r.Hash)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Node %d. Hash: %s\n", i, resp.Message)
 	}
 
 	return
