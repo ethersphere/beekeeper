03bc4e4e1634d27203e4f17592e59500556a41a7
svetomir <svetomirsmiljkovic@gmail.com>
2020-05-29 18:07:29 +0200

    Rewrite pingpong check to use streams

--- pkg/check/pingpong/pingpong.go
+++ pkg/check/pingpong/pingpong.go
@@ -3,32 +3,85 @@ package pingpong
 import (
 	"context"
 	"fmt"
+	"sort"
+	"sync"
+	"time"
 
+	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/beekeeper/pkg/bee"
 )
 
 // Check executes ping from all nodes to all other nodes in the cluster
 func Check(cluster bee.Cluster) (err error) {
 	ctx := context.Background()
-	for i, n := range cluster.Nodes {
-		o, err := n.Overlay(ctx)
-		if err != nil {
-			return fmt.Errorf("node %d: %w", i, err)
-		}
+	t1 := time.Now()
+	var wg sync.WaitGroup
+	for i, node := range cluster.Nodes {
+		wg.Add(1)
+		go func(i int, n bee.Node) {
+			defer wg.Done()
 
-		peers, err := n.Peers(ctx)
-		if err != nil {
-			return fmt.Errorf("node %d: %w", i, err)
-		}
-
-		for j, p := range peers {
-			rtt, err := n.Ping(ctx, p)
+			overlay, err := n.Overlay(ctx)
 			if err != nil {
-				return fmt.Errorf("node %d had error pinging peer %s: %w", i, p.String(), err)
+				fmt.Printf("node %d: %s\n", i, err)
 			}
-			fmt.Printf("Node %d. Peer %d RTT: %s. Node: %s Peer: %s \n", i, j, rtt, o.String(), p.String())
-		}
+
+			peers, err := n.Peers(ctx)
+			if err != nil {
+				fmt.Printf("node %d: %s\n", i, err)
+			}
+
+			var msgs []pingStreamMsg
+			for m := range pingStream(ctx, n, peers) {
+				msgs = append(msgs, m)
+			}
+
+			sort.SliceStable(msgs, func(i, j int) bool {
+				return msgs[i].Index < msgs[j].Index
+			})
+
+			for j, m := range msgs {
+				if m.Error != nil {
+					fmt.Printf("node %d had error pinging peer %s: %s\n", i, peers[j], m.Error)
+				}
+				fmt.Printf("Node %d. Peer %d RTT: %s. Node: %s Peer: %s \n", i, j, m.RTT, overlay, peers[j])
+			}
+		}(i, node)
 	}
+	wg.Wait()
+
+	fmt.Println("Elapsed: ", time.Since(t1))
 
 	return
+}
+
+type pingStreamMsg struct {
+	RTT   string
+	Index int
+	Error error
+}
+
+func pingStream(ctx context.Context, node bee.Node, peers []swarm.Address) <-chan pingStreamMsg {
+	pingStream := make(chan pingStreamMsg)
+
+	var wg sync.WaitGroup
+	for i, p := range peers {
+		wg.Add(1)
+		go func(i int, n bee.Node, p swarm.Address) {
+			defer wg.Done()
+			rtt, err := n.Ping(ctx, p)
+			pingStream <- pingStreamMsg{
+				RTT:   rtt,
+				Index: i,
+				Error: err,
+			}
+		}(i, node, p)
+	}
+
+	go func() {
+		wg.Wait()
+		close(pingStream)
+	}()
+
+	return pingStream
 }
