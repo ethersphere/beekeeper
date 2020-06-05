cb74c354c453e8ff21c86c36161c43614a8217d1
Ivan Vandot <ivan@vandot.rs>
2020-06-05 18:48:57 +0200

    initial support for chaos scenarios

---
+++ cmd/beekeeper/cmd/turn_podkill.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethersphere/beekeeper/pkg/chaos"
+
+	"github.com/spf13/cobra"
+)
+
+func (c *command) initTurnPodkill(action string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "podkill",
+		Short: "podkill scenario",
+		Long:  `podkill scenario.`,
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			ctx := context.Background()
+			kubeconfig := c.config.GetString(optionNameKubeConfig)
+			mode := c.config.GetString(optionNameMode)
+			value := c.config.GetString(optionNameValue)
+			namespace := c.config.GetString(optionNameChaosNamespace)
+			podname := c.config.GetString(optionNamePodname)
+			cron := c.config.GetString(optionNameCron)
+
+			err = chaos.PodKill(ctx, kubeconfig, action, mode, value, namespace, podname, cron)
+			if err != nil {
+				return err
+			}
+			if action == "create" {
+				fmt.Printf("Turned on pod-kill-%s\n", mode)
+			} else {
+				fmt.Printf("Turned off pod-kill-%s\n", mode)
+			}
+			return
+		},
+		PreRunE: c.turnPreRunE,
+	}
+}
---
+++ pkg/chaos/podchaos.go
@@ -0,0 +1,85 @@
+package chaos
+
+import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+func podFailure(mode string, value string, namespace string, label string, podname string, duration string, cron string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "pingcap.com/v1alpha1",
+			"kind":       "PodChaos",
+			"metadata": map[string]interface{}{
+				"name":      "pod-failure-" + mode,
+				"namespace": "chaos-testing",
+			},
+			"spec": map[string]interface{}{
+				"action":   "pod-failure",
+				"mode":     mode,
+				"value":    value,
+				"duration": duration,
+				"selector": map[string]interface{}{
+					"namespaces": []string{
+						namespace},
+					"labelSelectors": map[string]interface{}{
+						label: podname,
+					},
+				},
+				"scheduler": map[string]interface{}{
+					"cron": "@every " + cron,
+				},
+			},
+		},
+	}
+}
+
+func podKillCron(mode string, value string, namespace string, label string, podname string, cron string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "pingcap.com/v1alpha1",
+			"kind":       "PodChaos",
+			"metadata": map[string]interface{}{
+				"name":      "pod-kill-" + mode,
+				"namespace": "chaos-testing",
+			},
+			"spec": map[string]interface{}{
+				"action": "pod-kill",
+				"mode":   mode,
+				"value":  value,
+				"selector": map[string]interface{}{
+					"namespaces": []string{
+						namespace},
+					"labelSelectors": map[string]interface{}{
+						label: podname,
+					},
+				},
+				"scheduler": map[string]interface{}{
+					"cron": "@every " + cron,
+				},
+			},
+		},
+	}
+}
+
+func podKill(mode string, value string, namespace string, label string, podname string) *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "pingcap.com/v1alpha1",
+			"kind":       "PodChaos",
+			"metadata": map[string]interface{}{
+				"name":      "pod-kill-" + mode,
+				"namespace": "chaos-testing",
+			},
+			"spec": map[string]interface{}{
+				"action": "pod-kill",
+				"mode":   mode,
+				"value":  value,
+				"selector": map[string]interface{}{
+					"namespaces": []string{
+						namespace},
+					"labelSelectors": map[string]interface{}{
+						label: podname,
+					},
+				},
+			},
+		},
+	}
+}
