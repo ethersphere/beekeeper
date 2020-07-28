80717330bbad97387e35593a61e58698252929e5
svetomir <svetomirsmiljkovic@gmail.com>
2020-07-28 15:04:19 +0200

    Update localpinning description and parameter conditions

--- cmd/beekeeper/cmd/check_localpinning.go
+++ cmd/beekeeper/cmd/check_localpinning.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/check/localpinning"
 	"github.com/ethersphere/beekeeper/pkg/random"
@@ -21,9 +23,20 @@ func (c *command) initCheckLocalPinning() *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "localpinning",
-		Short: "localpinning",
-		Long:  `localpinning.`,
+		Short: "Checks local pinning ability of the cluster.",
+		Long: `Checks local pinning ability of the cluster.
+It first uploads small reference file to a random node and tries downloading it from another node.
+If everything goes well with the reference file, it uploads new small file to another random node and pins it.
+It uploads large file to the same node, and tries downloading small file from other random node. This step can be repeated multiple times (large-file-count).
+If everything goes well, it unpins the file.`,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			if c.config.GetInt(optionNameSmallFileDiskRatio) >= c.config.GetInt(optionNameLargeFileDiskRatio) || c.config.GetFloat64(optionNameSmallFileDiskRatio) >= 1 {
+				return errors.New("bad parameters: small-file-disk-ratio must be less than large-file-disk-ratio, and less than 1")
+			}
+			if c.config.GetInt64(optionNameDBCapacity) <= 0 || c.config.GetInt(optionNameLargeFileCount) <= 0 || c.config.GetFloat64(optionNameLargeFileDiskRatio) <= 0 || c.config.GetFloat64(optionNameSmallFileDiskRatio) <= 0 {
+				return errors.New("bad parameters: db-capacity, large-file-count, large-file-disk-ratio and small-file-disk-ratio must be greater than 0")
+			}
+
 			cluster, err := bee.NewCluster(bee.ClusterOptions{
 				APIScheme:               c.config.GetString(optionNameAPIScheme),
 				APIHostnamePattern:      c.config.GetString(optionNameAPIHostnamePattern),
