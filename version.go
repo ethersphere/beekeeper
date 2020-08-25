c180cf34877b4887d301b09b7408561b391aee18
Svetomir Smiljkovic <svetomir@ethswarm.org>
2020-08-25 14:15:03 +0200

    Bump version (#41)

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package beekeeper
 
 var (
-	version = "0.2.16" // manually set semantic version number
+	version = "0.2.17" // manually set semantic version number
 	commit  string     // automatically set git commit hash
 
 	// Version TODO
