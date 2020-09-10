d9f1b62b05625ce3c22554755fa429b43e063c0a
svetomir <svetomirsmiljkovic@gmail.com>
2020-09-10 16:09:06 +0200

    Add initial validation

--- cmd/beekeeper/cmd/check_balances.go
+++ cmd/beekeeper/cmd/check_balances.go
@@ -1,19 +1,38 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/check/balances"
+	"github.com/ethersphere/beekeeper/pkg/random"
 	"github.com/prometheus/client_golang/prometheus/push"
 
 	"github.com/spf13/cobra"
 )
 
 func (c *command) initCheckBalances() *cobra.Command {
-	return &cobra.Command{
+	const (
+		optionNameUploadNodeCount = "upload-node-count"
+		optionNameFileName        = "file-name"
+		optionNameFileSize        = "file-size"
+		optionNameSeed            = "seed"
+		optionNameDryRun          = "dry-run"
+	)
+
+	var (
+		dryRun bool
+	)
+
+	cmd := &cobra.Command{
 		Use:   "balances",
 		Short: "Executes balances check",
 		Long:  `Executes balances check.`,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			if c.config.GetInt(optionNameUploadNodeCount) > c.config.GetInt(optionNameNodeCount) {
+				return errors.New("bad parameters: upload-node-count must be less or equal to node-count")
+			}
+
 			cluster, err := bee.NewCluster(bee.ClusterOptions{
 				APIScheme:               c.config.GetString(optionNameAPIScheme),
 				APIHostnamePattern:      c.config.GetString(optionNameAPIHostnamePattern),
@@ -33,8 +52,34 @@ func (c *command) initCheckBalances() *cobra.Command {
 
 			pusher := push.New(c.config.GetString(optionNamePushGateway), c.config.GetString(optionNameNamespace))
 
-			return balances.Check(cluster, pusher, c.config.GetBool(optionNamePushMetrics))
+			if dryRun {
+				return balances.DryRunCheck(cluster)
+			}
+
+			var seed int64
+			if cmd.Flags().Changed("seed") {
+				seed = c.config.GetInt64(optionNameSeed)
+			} else {
+				seed = random.Int64()
+			}
+
+			fileSize := round(c.config.GetFloat64(optionNameFileSize) * 1024 * 1024)
+
+			return balances.Check(cluster, balances.Options{
+				UploadNodeCount: c.config.GetInt(optionNameUploadNodeCount),
+				FileName:        c.config.GetString(optionNameFileName),
+				FileSize:        fileSize,
+				Seed:            seed,
+			}, pusher, c.config.GetBool(optionNamePushMetrics))
 		},
 		PreRunE: c.checkPreRunE,
 	}
+
+	cmd.Flags().IntP(optionNameUploadNodeCount, "u", 1, "number of nodes to upload files to")
+	cmd.Flags().String(optionNameFileName, "file", "file name template")
+	cmd.Flags().Float64(optionNameFileSize, 1, "file size in MB")
+	cmd.Flags().Int64P(optionNameSeed, "s", 0, "seed for generating files; if not set, will be random")
+	cmd.Flags().BoolVar(&dryRun, optionNameDryRun, false, "don't upload and download files, just validate")
+
+	return cmd
 }
--- pkg/check/balances/balances.go
+++ pkg/check/balances/balances.go
@@ -1,15 +1,151 @@
 package balances
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"math/rand"
 
+	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/beekeeper/pkg/bee"
+	"github.com/ethersphere/beekeeper/pkg/random"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
+// Options represents balances check options
+type Options struct {
+	UploadNodeCount int
+	FileName        string
+	FileSize        int64
+	Seed            int64
+}
+
 // Check executes balances check
-func Check(cluster bee.Cluster, pusher *push.Pusher, pushMetrics bool) (err error) {
-	fmt.Println("balances")
+func Check(c bee.Cluster, o Options, pusher *push.Pusher, pushMetrics bool) (err error) {
+	ctx := context.Background()
+	rnd := random.PseudoGenerator(o.Seed)
+	fmt.Printf("Seed: %d\n", o.Seed)
+
+	overlays, err := c.Overlays(ctx)
+	if err != nil {
+		return err
+	}
+
+	balances, err := c.Balances(ctx)
+	if err != nil {
+		return err
+	}
+	if err := validateBalances(overlays, balances); err != nil {
+		return fmt.Errorf("initial validation")
+	}
+
+	for i := 0; i < o.UploadNodeCount; i++ {
+		// validate balances before uploading a file
+		b, err := c.Balances(ctx)
+		if err != nil {
+			return err
+		}
+		if err := validateBalances(overlays, b); err != nil {
+			return fmt.Errorf("invalid balances before uploading a file")
+		}
+
+		// upload file to random node
+		uIndex := rnd.Intn(c.Size())
+		file := bee.NewRandomFile(rnd, fmt.Sprintf("%s-%d", o.FileName, uIndex), o.FileSize)
+		if err := c.Nodes[uIndex].UploadFile(ctx, &file); err != nil {
+			return fmt.Errorf("node %d: %w", uIndex, err)
+		}
+		fmt.Printf("Node %d. File uploaded successfully. Node: %s File: %s\n", uIndex, overlays[uIndex].String(), file.Address().String())
+
+		// validate balances after uploading a file
+		b, err = c.Balances(ctx)
+		if err != nil {
+			return err
+		}
+		if err := validateBalances(overlays, b); err != nil {
+			return fmt.Errorf("invalid balances after uploading a file")
+		}
+
+		// download file from random node
+		dIndex := randomIndex(rnd, c.Size(), uIndex)
+		size, hash, err := c.Nodes[dIndex].DownloadFile(ctx, file.Address())
+		if err != nil {
+			return fmt.Errorf("node %d: %w", dIndex, err)
+		}
+		if !bytes.Equal(file.Hash(), hash) {
+			return fmt.Errorf("Node %d. File not retrieved successfully. Uploaded size: %d Downloaded size: %d Node: %s File: %s", dIndex, file.Size(), size, overlays[dIndex].String(), file.Address().String())
+		}
+		fmt.Printf("Node %d. File downloaded successfully. Node: %s File: %s\n", dIndex, overlays[dIndex].String(), file.Address().String())
+
+		b, err = c.Balances(ctx)
+		if err != nil {
+			return err
+		}
+		if err := validateBalances(overlays, b); err != nil {
+			return fmt.Errorf("invalid balances after downloading a file")
+		}
+	}
+
+	return
+}
+
+// DryRunCheck executes balances validation check without files uploading/downloading
+func DryRunCheck(c bee.Cluster) (err error) {
+	ctx := context.Background()
+
+	overlays, err := c.Overlays(ctx)
+	if err != nil {
+		return err
+	}
+
+	balances, err := c.Balances(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := validateBalances(overlays, balances); err != nil {
+		return fmt.Errorf("invalid balances")
+	}
+	fmt.Printf("valid balances\n")
+
+	return
+}
+
+// validateBalances checks if balances are valid
+func validateBalances(overlays []swarm.Address, balances []bee.Balances) (err error) {
+	initialValidation := true
+	for _, b := range balances {
+		if len(b.Balances) != 0 {
+			initialValidation = false
+		}
+	}
+	if initialValidation {
+		return
+	}
+
+	ob := make(map[string][]bee.Balance)
+	for i := 0; i < len(overlays); i++ {
+		ob[overlays[i].String()] = balances[i].Balances
+	}
+
+	for k, v := range ob {
+		for _, b := range v {
+			fmt.Printf("Node %s has balance %d with node %s\n", k, b.Balance, b.Peer)
+		}
+	}
+
+	return
+}
+
+// randomIndex finds random index <max and not equal to unallowed
+func randomIndex(rnd *rand.Rand, max int, unallowed int) (index int) {
+	found := false
+	for !found {
+		index = rnd.Intn(max)
+		if index != unallowed {
+			found = true
+		}
+	}
 
 	return
 }
