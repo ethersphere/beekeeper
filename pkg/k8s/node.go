4d802d7be535a7d9462a602e05b951e554d907d5
svetomir <svetomirsmiljkovic@gmail.com>
2020-10-20 11:49:48 +0200

    Add debug svc and ingress

--- pkg/k8s/node.go
+++ pkg/k8s/node.go
@@ -19,7 +19,8 @@ import (
 const (
 	libp2pKeys = `bee-0: {"address":"aa6675fb77f3f84304a00d5ea09902d8a500364091a457cf21e05a41875d48f7","crypto":{"cipher":"aes-128-ctr","ciphertext":"93effebd3f015f496367e14218cb26d22de8f899e1d7b7686deb6ab43c876ea5","cipherparams":{"iv":"627434462c2f960d37338022d27fc92e"},"kdf":"scrypt","kdfparams":{"n":32768,"r":8,"p":1,"dklen":32,"salt":"a59e72e725fe3de25dd9c55aa55a93ed0e9090b408065a7204e2f505653acb70"},"mac":"dfb1e7ad93252928a7ff21ea5b65e8a4b9bda2c2e09cb6a8ac337da7a3568b8c"},"version":3}
 bee-1: {"address":"03348ecf3adae1d05dc16e475a83c94e49e28a4d3c7db5eccbf5ca4ea7f688ddcdfe88acbebef2037c68030b1a0a367a561333e5c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470","crypto":{"cipher":"aes-128-ctr","ciphertext":"0d0ff25e9b03292e622c5a09ec00c2acb7ff5882f02dd2f00a26ac6d3292a434","cipherparams":{"iv":"cd4082caf63320b306fe885796ba224f"},"kdf":"scrypt","kdfparams":{"n":32768,"r":8,"p":1,"dklen":32,"salt":"a4d63d56c539eb3eff2a235090127486722fa2c836cf735d50d673b730cebc3f"},"mac":"aad40da9c1e742e2b01bb8f76ba99ace97ccb0539cea40e31eb6b9bb64a3f36a"},"version":3}`
-	initCmd = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev); mkdir -p /home/bee/.bee/keys; chown -R 999:999 /home/bee/.bee/keys; export KEY=$(cat /tmp/bee/libp2p.map | grep bee-${INDEX}: | cut -d' ' -f2); if [ -z "${KEY}" ]; then exit 0; fi; printf '%s' "${KEY}" > /home/bee/.bee/keys/libp2p.key; echo 'node initialization done';`
+	initCmd  = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev); mkdir -p /home/bee/.bee/keys; chown -R 999:999 /home/bee/.bee/keys; export KEY=$(cat /tmp/bee/libp2p.map | grep bee-${INDEX}: | cut -d' ' -f2); if [ -z "${KEY}" ]; then exit 0; fi; printf '%s' "${KEY}" > /home/bee/.bee/keys/libp2p.key; echo 'node initialization done';`
+	portHTTP = 80
 )
 
 // NodeStartOptions ...
@@ -27,24 +28,27 @@ type NodeStartOptions struct {
 	// Bee configuration
 	Config Config
 	// Kubernetes configuration
-	Name                string
-	Namespace           string
-	Annotations         map[string]string
-	Labels              map[string]string
-	LimitCPU            string
-	LimitMemory         string
-	Image               string
-	ImagePullPolicy     string
-	IngressAnnotations  map[string]string
-	IngressClass        string
-	IngressHost         string
-	NodeSelector        map[string]string
-	PodManagementPolicy string
-	RestartPolicy       string
-	RequestCPU          string
-	RequestMemory       string
-	Selector            map[string]string
-	UpdateStrategy      string
+	Name                    string
+	Namespace               string
+	Annotations             map[string]string
+	Labels                  map[string]string
+	LimitCPU                string
+	LimitMemory             string
+	Image                   string
+	ImagePullPolicy         string
+	IngressAnnotations      map[string]string
+	IngressClass            string
+	IngressHost             string
+	IngressDebugAnnotations map[string]string
+	IngressDebugClass       string
+	IngressDebugHost        string
+	NodeSelector            map[string]string
+	PodManagementPolicy     string
+	RestartPolicy           string
+	RequestCPU              string
+	RequestMemory           string
+	Selector                map[string]string
+	UpdateStrategy          string
 }
 
 // NodeStart ...
@@ -103,38 +107,75 @@ func (c Client) NodeStart(ctx context.Context, o NodeStartOptions) (err error) {
 	}
 	fmt.Printf("serviceaccount %s is set in namespace %s\n", svcAccount, o.Namespace)
 
-	// http service
-	httpSvc := o.Name
-	if err := c.Service.Set(ctx, httpSvc, o.Namespace, service.Options{
+	// api service
+	apiSvc := fmt.Sprintf("%s-api", o.Name)
+	if err := c.Service.Set(ctx, apiSvc, o.Namespace, service.Options{
 		Annotations: o.Annotations,
 		Labels:      o.Labels,
-		Ports: []service.Port{{
-			Name:       "http",
-			Protocol:   "TCP",
-			Port:       80,
-			TargetPort: "api",
-		}},
+		Ports: []service.Port{
+			{
+				Name:       "api",
+				Protocol:   "TCP",
+				Port:       portHTTP,
+				TargetPort: "api",
+			},
+		},
 		Selector: o.Selector,
 		Type:     "ClusterIP",
 	}); err != nil {
 		return fmt.Errorf("set service in namespace %s: %s", o.Namespace, err)
 	}
-	fmt.Printf("service %s is set in namespace %s\n", httpSvc, o.Namespace)
+	fmt.Printf("service %s is set in namespace %s\n", apiSvc, o.Namespace)
 
-	// http service's ingress
-	httpIn := o.Name
-	if err := c.Ingress.Set(ctx, httpIn, o.Namespace, ingress.Options{
+	// api service's ingress
+	apiIn := o.Name
+	if err := c.Ingress.Set(ctx, apiIn, o.Namespace, ingress.Options{
 		Annotations: mergeMaps(o.Annotations, o.IngressAnnotations),
 		Labels:      o.Labels,
 		Class:       o.IngressClass,
 		Host:        o.IngressHost,
-		ServiceName: httpSvc,
-		ServicePort: "http",
+		ServiceName: apiSvc,
+		ServicePort: "api",
 		Path:        "/",
 	}); err != nil {
 		return fmt.Errorf("set ingress in namespace %s: %s", o.Namespace, err)
 	}
-	fmt.Printf("ingress %s is set in namespace %s\n", httpIn, o.Namespace)
+	fmt.Printf("ingress %s is set in namespace %s\n", apiIn, o.Namespace)
+
+	// debug service
+	debugSvc := fmt.Sprintf("%s-debug", o.Name)
+	if err := c.Service.Set(ctx, debugSvc, o.Namespace, service.Options{
+		Annotations: o.Annotations,
+		Labels:      o.Labels,
+		Ports: []service.Port{
+			{
+				Name:       "debug",
+				Protocol:   "TCP",
+				Port:       portDebug,
+				TargetPort: "debug",
+			},
+		},
+		Selector: o.Selector,
+		Type:     "ClusterIP",
+	}); err != nil {
+		return fmt.Errorf("set service in namespace %s: %s", o.Namespace, err)
+	}
+	fmt.Printf("service %s is set in namespace %s\n", debugSvc, o.Namespace)
+
+	// debug service's ingress
+	debugIn := fmt.Sprintf("%s-debug", o.Name)
+	if err := c.Ingress.Set(ctx, debugIn, o.Namespace, ingress.Options{
+		Annotations: mergeMaps(o.Annotations, o.IngressDebugAnnotations),
+		Labels:      o.Labels,
+		Class:       o.IngressDebugClass,
+		Host:        o.IngressDebugHost,
+		ServiceName: debugSvc,
+		ServicePort: "debug",
+		Path:        "/",
+	}); err != nil {
+		return fmt.Errorf("set ingress in namespace %s: %s", o.Namespace, err)
+	}
+	fmt.Printf("ingress %s is set in namespace %s\n", debugIn, o.Namespace)
 
 	// headless service
 	headlessSvc := fmt.Sprintf("%s-headless", o.Name)
