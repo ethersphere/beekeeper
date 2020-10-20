38c7b7ae2f25063dca7c1a7351247534a7574dba
svetomir <svetomirsmiljkovic@gmail.com>
2020-10-20 16:19:15 +0200

    Create helper functions for statefulset

--- pkg/k8s/node.go
+++ pkg/k8s/node.go
@@ -19,7 +19,19 @@ import (
 const (
 	libp2pKeys = `bee-0: {"address":"aa6675fb77f3f84304a00d5ea09902d8a500364091a457cf21e05a41875d48f7","crypto":{"cipher":"aes-128-ctr","ciphertext":"93effebd3f015f496367e14218cb26d22de8f899e1d7b7686deb6ab43c876ea5","cipherparams":{"iv":"627434462c2f960d37338022d27fc92e"},"kdf":"scrypt","kdfparams":{"n":32768,"r":8,"p":1,"dklen":32,"salt":"a59e72e725fe3de25dd9c55aa55a93ed0e9090b408065a7204e2f505653acb70"},"mac":"dfb1e7ad93252928a7ff21ea5b65e8a4b9bda2c2e09cb6a8ac337da7a3568b8c"},"version":3}
 bee-1: {"address":"03348ecf3adae1d05dc16e475a83c94e49e28a4d3c7db5eccbf5ca4ea7f688ddcdfe88acbebef2037c68030b1a0a367a561333e5c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470","crypto":{"cipher":"aes-128-ctr","ciphertext":"0d0ff25e9b03292e622c5a09ec00c2acb7ff5882f02dd2f00a26ac6d3292a434","cipherparams":{"iv":"cd4082caf63320b306fe885796ba224f"},"kdf":"scrypt","kdfparams":{"n":32768,"r":8,"p":1,"dklen":32,"salt":"a4d63d56c539eb3eff2a235090127486722fa2c836cf735d50d673b730cebc3f"},"mac":"aad40da9c1e742e2b01bb8f76ba99ace97ccb0539cea40e31eb6b9bb64a3f36a"},"version":3}`
-	initCmd  = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev); mkdir -p /home/bee/.bee/keys; chown -R 999:999 /home/bee/.bee/keys; export KEY=$(cat /tmp/bee/libp2p.map | grep bee-${INDEX}: | cut -d' ' -f2); if [ -z "${KEY}" ]; then exit 0; fi; printf '%s' "${KEY}" > /home/bee/.bee/keys/libp2p.key; echo 'node initialization done';`
+	initCmd      = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev); mkdir -p /home/bee/.bee/keys; chown -R 999:999 /home/bee/.bee/keys; export KEY=$(cat /tmp/bee/libp2p.map | grep bee-${INDEX}: | cut -d' ' -f2); if [ -z "${KEY}" ]; then exit 0; fi; printf '%s' "${KEY}" > /home/bee/.bee/keys/libp2p.key; echo 'node initialization done';`
+	initSwarmCmd = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev);
+mkdir -p /home/bee/.bee/keys;
+chown -R 999:999 /home/bee/.bee/keys;
+export KEY=$(cat /tmp/bee/swarm.map | grep bee-${INDEX}: | cut -d' ' -f2);
+if [ -z "${KEY}" ]; then exit 0; fi;
+printf '%s' "${KEY}" > /home/bee/.bee/keys/swarm.key;
+echo 'node initialization done';`
+	initP2PFixedPortsCmd = `export INDEX=$(echo $(hostname) | rev | cut -d'-' -f 1 | rev);
+NAT_PORT=$(( {{ .Values.p2pFixedPort.nodePortStart }} + INDEX ));
+cp -p /tmp/.bee.yaml /home/bee/.bee.yaml;
+printf 'nat-addr: :%s\n' "${NAT_PORT}" >> /home/bee/.bee.yaml;
+echo 'node initialization done';`
 	portHTTP = 80
 )
 
@@ -232,75 +244,9 @@ func (c Client) NodeStart(ctx context.Context, o NodeStartOptions) (err error) {
 	// statefulset
 	sSet := o.Name
 	if err := c.StatefulSet.Set(ctx, sSet, o.Namespace, statefulset.Options{
-		Annotations: o.Annotations,
-		InitContainers: []statefulset.InitContainer{{
-			Name:    "init-libp2p",
-			Image:   "busybox:1.28",
-			Command: []string{"sh", "-c", initCmd},
-			VolumeMounts: []statefulset.VolumeMount{
-				{
-					Name:      "bee-libp2p",
-					MountPath: "/tmp/bee",
-				},
-				{
-					Name:      "data",
-					MountPath: "home/bee/.bee",
-				},
-			},
-		}},
-		Containers: []statefulset.Container{{
-			Name:            sSet,
-			Image:           o.Image,
-			ImagePullPolicy: o.ImagePullPolicy,
-			Command:         []string{"bee", "start", "--config=.bee.yaml"},
-			Ports: []statefulset.Port{
-				{
-					Name:          "api",
-					ContainerPort: portAPI,
-					Protocol:      "TCP",
-				},
-				{
-					Name:          "debug",
-					ContainerPort: portDebug,
-					Protocol:      "TCP",
-				},
-				{
-					Name:          "p2p",
-					ContainerPort: portP2P,
-					Protocol:      "TCP",
-				},
-			},
-			LivenessProbe: statefulset.Probe{
-				Path: "/health",
-				Port: "debug",
-			},
-			ReadinessProbe: statefulset.Probe{
-				Path: "/readiness",
-				Port: "debug",
-			},
-			Resources: statefulset.Resources{
-				LimitCPU:      o.LimitCPU,
-				LimitMemory:   o.LimitMemory,
-				RequestCPU:    o.RequestCPU,
-				RequestMemory: o.RequestMemory,
-			},
-			SecurityContext: statefulset.SecurityContext{
-				AllowPrivilegeEscalation: false,
-				RunAsUser:                999,
-			},
-			VolumeMounts: []statefulset.VolumeMount{
-				{
-					Name:      "config",
-					MountPath: "/home/bee/.bee.yaml",
-					SubPath:   ".bee.yaml",
-					ReadOnly:  true,
-				},
-				{
-					Name:      "data",
-					MountPath: "home/bee/.bee",
-				},
-			},
-		}},
+		Annotations:         o.Annotations,
+		InitContainers:      c.setInitContainers(),
+		Containers:          c.setContainers(sSet, o.Image, o.ImagePullPolicy, o.LimitCPU, o.LimitMemory, o.RequestCPU, o.RequestMemory, portAPI, portDebug, portP2P),
 		Labels:              o.Labels,
 		NodeSelector:        o.NodeSelector,
 		PodManagementPolicy: o.PodManagementPolicy,
@@ -315,27 +261,7 @@ func (c Client) NodeStart(ctx context.Context, o NodeStartOptions) (err error) {
 		UpdateStrategy: statefulset.UpdateStrategy{
 			Type: o.UpdateStrategy,
 		},
-		Volumes: []statefulset.Volume{
-			{
-				ConfigMap: &statefulset.ConfigMapVolume{
-					Name:          "config",
-					ConfigMapName: configCM,
-					DefaultMode:   420,
-				},
-			},
-			{EmptyDir: &statefulset.EmptyDirVolume{
-				Name: "data",
-			}},
-			{Secret: &statefulset.SecretVolume{
-				Name:        libp2pSecret,
-				SecretName:  libp2pSecret,
-				DefaultMode: 420,
-				Items: []statefulset.Item{{
-					Key:   "libp2pKeys",
-					Value: "libp2p.map",
-				}},
-			}},
-		},
+		Volumes: c.setVolumes(configCM, libp2pSecret),
 	}); err != nil {
 		return fmt.Errorf("set statefulset in namespace %s: %s", o.Namespace, err)
 	}
@@ -343,6 +269,137 @@ func (c Client) NodeStart(ctx context.Context, o NodeStartOptions) (err error) {
 
 	fmt.Printf("node %s started in namespace %s\n", o.Name, o.Namespace)
 	return
+}
+
+func (c Client) setInitContainers() []statefulset.InitContainer {
+	return []statefulset.InitContainer{
+		{
+			Name:    "init-libp2p",
+			Image:   "busybox:1.28",
+			Command: []string{"sh", "-c", initCmd},
+			VolumeMounts: []statefulset.VolumeMount{
+				{
+					Name:      "bee-libp2p",
+					MountPath: "/tmp/bee",
+				},
+				{
+					Name:      "data",
+					MountPath: "home/bee/.bee",
+				},
+			},
+		},
+		// {
+		// 	Name:    "init-swarm",
+		// 	Image:   "busybox:1.28",
+		// 	Command: []string{"sh", "-c", initSwarmCmd},
+		// 	VolumeMounts: []statefulset.VolumeMount{
+		// 		{
+		// 			Name:      "bee-swarm",
+		// 			MountPath: "/tmp/bee",
+		// 		},
+		// 		{
+		// 			Name:      "data",
+		// 			MountPath: "home/bee/.bee",
+		// 		},
+		// 	},
+		// },
+		// {
+		// 	Name:    "init-natport",
+		// 	Image:   "busybox:1.28",
+		// 	Command: []string{"sh", "-c", initP2PFixedPortsCmd},
+		// 	VolumeMounts: []statefulset.VolumeMount{
+		// 		{
+		// 			Name:      "config-file",
+		// 			MountPath: "/home/bee",
+		// 		},
+		// 		{
+		// 			Name:      "config",
+		// 			MountPath: "/tmp/.bee.yaml",
+		// 			SubPath:   ".bee.yaml",
+		// 		},
+		// 	},
+		// },
+	}
+}
+
+func (c Client) setContainers(name, image, imagePullPolicy, limitCPU, limitMemory, requestCPU, requestMemory string, portAPI, portDebug, portP2P int32) []statefulset.Container {
+	return []statefulset.Container{{
+		Name:            name,
+		Image:           image,
+		ImagePullPolicy: imagePullPolicy,
+		Command:         []string{"bee", "start", "--config=.bee.yaml"},
+		Ports: []statefulset.Port{
+			{
+				Name:          "api",
+				ContainerPort: portAPI,
+				Protocol:      "TCP",
+			},
+			{
+				Name:          "debug",
+				ContainerPort: portDebug,
+				Protocol:      "TCP",
+			},
+			{
+				Name:          "p2p",
+				ContainerPort: portP2P,
+				Protocol:      "TCP",
+			},
+		},
+		LivenessProbe: statefulset.Probe{
+			Path: "/health",
+			Port: "debug",
+		},
+		ReadinessProbe: statefulset.Probe{
+			Path: "/readiness",
+			Port: "debug",
+		},
+		Resources: statefulset.Resources{
+			LimitCPU:      limitCPU,
+			LimitMemory:   limitMemory,
+			RequestCPU:    requestCPU,
+			RequestMemory: requestMemory,
+		},
+		SecurityContext: statefulset.SecurityContext{
+			AllowPrivilegeEscalation: false,
+			RunAsUser:                999,
+		},
+		VolumeMounts: []statefulset.VolumeMount{
+			{
+				Name:      "config",
+				MountPath: "/home/bee/.bee.yaml",
+				SubPath:   ".bee.yaml",
+				ReadOnly:  true,
+			},
+			{
+				Name:      "data",
+				MountPath: "home/bee/.bee",
+			},
+		},
+	}}
+}
+
+func (c Client) setVolumes(configCM, libp2pSecret string) []statefulset.Volume {
+	return []statefulset.Volume{
+		{
+			ConfigMap: &statefulset.ConfigMapVolume{
+				Name:          "config",
+				ConfigMapName: configCM,
+				DefaultMode:   420,
+			},
+		},
+		{EmptyDir: &statefulset.EmptyDirVolume{
+			Name: "data",
+		}},
+		{Secret: &statefulset.SecretVolume{
+			Name:        libp2pSecret,
+			SecretName:  libp2pSecret,
+			DefaultMode: 420,
+			Items: []statefulset.Item{{
+				Key:   "libp2pKeys",
+				Value: "libp2p.map",
+			}},
+		}},
+	}
 }
 
 func mergeMaps(a, b map[string]string) map[string]string {
