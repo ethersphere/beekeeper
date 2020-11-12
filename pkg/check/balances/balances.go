828991ea2bdd1a6c04a89b9fbf2cdf5bbd16fd07
svetomir <svetomirsmiljkovic@gmail.com>
2020-11-12 17:15:06 +0100

    Use dynamic cluster in balances check

--- pkg/check/balances/balances.go
+++ pkg/check/balances/balances.go
@@ -1,12 +1,15 @@
 package balances
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/beekeeper/pkg/bee"
+	"github.com/ethersphere/beekeeper/pkg/random"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
@@ -22,92 +25,95 @@ type Options struct {
 
 // Check executes balances check
 func Check(c *bee.DynamicCluster, o Options, pusher *push.Pusher, pushMetrics bool) (err error) {
-	// ctx := context.Background()
-	// rnd := random.PseudoGenerator(o.Seed)
-	// fmt.Printf("Seed: %d\n", o.Seed)
+	ctx := context.Background()
+	rnd := random.PseudoGenerator(o.Seed)
+	fmt.Printf("Seed: %d\n", o.Seed)
 
-	// ng := c.NodeGroup(o.NodeGroup)
-	// overlays, err := ng.Overlays(ctx)
-	// if err != nil {
-	// 	return err
-	// }
+	ng := c.NodeGroup(o.NodeGroup)
+	overlays, err := ng.Overlays(ctx)
+	if err != nil {
+		return err
+	}
 
-	// // Initial balances validation
-	// balances, err := ng.Balances(ctx)
-	// if err != nil {
-	// 	return err
-	// }
-	// if err := validateBalances(overlays, balances); err != nil {
-	// 	return fmt.Errorf("invalid initial balances: %s", err.Error())
-	// }
-	// fmt.Println("Balances are valid")
+	// Initial balances validation
+	balances, err := ng.Balances(ctx)
+	if err != nil {
+		return err
+	}
+	if err := validateBalances(overlays, balances); err != nil {
+		return fmt.Errorf("invalid initial balances: %s", err.Error())
+	}
+	fmt.Println("Balances are valid")
 
-	// var previousBalances map[string]map[string]int
-	// for i := 0; i < o.UploadNodeCount; i++ {
-	// 	// upload file to random node
-	// 	uIndex := rnd.Intn(c.Size())
-	// 	file := bee.NewRandomFile(rnd, fmt.Sprintf("%s-%d", o.FileName, uIndex), o.FileSize)
-	// 	if err := c.Nodes[uIndex].UploadFile(ctx, &file, false); err != nil {
-	// 		return fmt.Errorf("node %d: %w", uIndex, err)
-	// 	}
-	// 	fmt.Printf("File %s uploaded successfully to node %s\n", file.Address().String(), overlays[uIndex].String())
+	var previousBalances bee.NodeGroupBalances
+	sortedNodes := ng.NodesSorted()
+	for i := 0; i < o.UploadNodeCount; i++ {
+		// upload file to random node
+		uIndex := rnd.Intn(c.Size())
+		uNode := sortedNodes[uIndex]
+		file := bee.NewRandomFile(rnd, fmt.Sprintf("%s-%d", o.FileName, uIndex), o.FileSize)
+		if err := ng.Node(uNode).UploadFile(ctx, &file, false); err != nil {
+			return fmt.Errorf("node %s: %w", uNode, err)
+		}
+		fmt.Printf("File %s uploaded successfully to node %s\n", file.Address().String(), overlays[uNode].String())
 
-	// 	// Validate balances after uploading a file
-	// 	previousBalances = balances
-	// 	for t := 0; t < 5; t++ {
-	// 		time.Sleep(2 * time.Duration(t) * time.Second)
+		// Validate balances after uploading a file
+		previousBalances = balances
+		for t := 0; t < 5; t++ {
+			time.Sleep(2 * time.Duration(t) * time.Second)
 
-	// 		balances, err = c.Balances(ctx)
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		balancesHaveChanged(balances, previousBalances)
+			balances, err = ng.Balances(ctx)
+			if err != nil {
+				return err
+			}
+			balancesHaveChanged(balances, previousBalances)
 
-	// 		err = validateBalances(overlays, balances)
-	// 		if err != nil {
-	// 			fmt.Printf("Invalid balances after uploading a file: %s\n", err.Error())
-	// 			fmt.Println("Retrying ...")
-	// 			continue
-	// 		}
+			err = validateBalances(overlays, balances)
+			if err != nil {
+				fmt.Printf("Invalid balances after uploading a file: %s\n", err.Error())
+				fmt.Println("Retrying ...")
+				continue
+			}
 
-	// 		fmt.Println("Balances are valid")
-	// 		break
-	// 	}
+			fmt.Println("Balances are valid")
+			break
+		}
 
-	// 	time.Sleep(time.Duration(o.WaitBeforeDownload) * time.Second)
-	// 	// download file from random node
-	// 	dIndex := randomIndex(rnd, c.Size(), uIndex)
-	// 	size, hash, err := c.Nodes[dIndex].DownloadFile(ctx, file.Address())
-	// 	if err != nil {
-	// 		return fmt.Errorf("node %d: %w", dIndex, err)
-	// 	}
-	// 	if !bytes.Equal(file.Hash(), hash) {
-	// 		return fmt.Errorf("File %s not retrieved successfully from node %s. Uploaded size: %d Downloaded size: %d", file.Address().String(), overlays[dIndex].String(), file.Size(), size)
-	// 	}
-	// 	fmt.Printf("File %s downloaded successfully from node %s\n", file.Address().String(), overlays[dIndex].String())
+		time.Sleep(time.Duration(o.WaitBeforeDownload) * time.Second)
+		// download file from random node
+		dIndex := randomIndex(rnd, c.Size(), uIndex)
+		dNode := sortedNodes[dIndex]
+		size, hash, err := ng.Node(dNode).DownloadFile(ctx, file.Address())
+		if err != nil {
+			return fmt.Errorf("node %s: %w", dNode, err)
+		}
+		if !bytes.Equal(file.Hash(), hash) {
+			return fmt.Errorf("File %s not retrieved successfully from node %s. Uploaded size: %d Downloaded size: %d", file.Address().String(), overlays[dNode].String(), file.Size(), size)
+		}
+		fmt.Printf("File %s downloaded successfully from node %s\n", file.Address().String(), overlays[dNode].String())
 
-	// 	// Validate balances after downloading a file
-	// 	previousBalances = balances
-	// 	for t := 0; t < 5; t++ {
-	// 		time.Sleep(2 * time.Duration(t) * time.Second)
+		// Validate balances after downloading a file
+		previousBalances = balances
+		for t := 0; t < 5; t++ {
+			time.Sleep(2 * time.Duration(t) * time.Second)
 
-	// 		balances, err = c.Balances(ctx)
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		balancesHaveChanged(balances, previousBalances)
+			balances, err = ng.Balances(ctx)
+			if err != nil {
+				return err
+			}
+			balancesHaveChanged(balances, previousBalances)
 
-	// 		err := validateBalances(overlays, balances)
-	// 		if err != nil {
-	// 			fmt.Printf("Invalid balances after downloading a file: %s\n", err.Error())
-	// 			fmt.Println("Retrying ...")
-	// 			continue
-	// 		}
+			err := validateBalances(overlays, balances)
+			if err != nil {
+				fmt.Printf("Invalid balances after downloading a file: %s\n", err.Error())
+				fmt.Println("Retrying ...")
+				continue
+			}
 
-	// 		fmt.Println("Balances are valid")
-	// 		break
-	// 	}
-	// }
+			fmt.Println("Balances are valid")
+			break
+		}
+	}
 
 	return
 }
@@ -158,7 +164,7 @@ func validateBalances(overlays map[string]swarm.Address, balances bee.NodeGroupB
 }
 
 // balancesHaveChanged checks if balances have changed
-func balancesHaveChanged(current, previous map[string]map[string]int) {
+func balancesHaveChanged(current, previous bee.NodeGroupBalances) {
 	for node, v := range current {
 		for peer, balance := range v {
 			if balance != previous[node][peer] {
