16b1e084cfb76908eae719f1a3e40aad430462f1
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-12-11 19:50:02 +0100

    Add addidtional pinning calls in the beeclient

--- pkg/beeclient/api/pinning.go
+++ pkg/beeclient/api/pinning.go
@@ -66,3 +66,105 @@ func (p *PinningService) UnpinChunk(ctx context.Context, a swarm.Address) (bool,
 
 	return true, nil
 }
+
+// PinBytes pins chunks for bytes upload
+func (p *PinningService) PinBytes(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodPost, "/pin/bytes/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// UnpinBytes unpins chunks for bytes upload
+func (p *PinningService) UnpinBytes(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodDelete, "/pin/bytes/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// PinFiles pins chunks for files upload
+func (p *PinningService) PinFiles(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodPost, "/pin/files/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// UnpinFiles unpins chunks for files upload
+func (p *PinningService) UnpinFiles(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodDelete, "/pin/files/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// PinBzz pins chunks for bzz upload
+func (p *PinningService) PinBzz(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodPost, "/pin/bzz/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// UnpinBzz unpins chunks for bzz upload
+func (p *PinningService) UnpinBzz(ctx context.Context, a swarm.Address) (bool, error) {
+	resp := struct {
+		Message string `json:"message,omitempty"`
+		Code    int    `json:"code,omitempty"`
+	}{}
+
+	err := p.client.requestJSON(ctx, http.MethodDelete, "/pin/bzz/"+a.String(), nil, &resp)
+	if err == ErrNotFound {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
