74b9b5ab9be2ccf243ed417fba504202ef9b5d56
svetomir <svetomirsmiljkovic@gmail.com>
2020-12-16 17:20:45 +0100

    Add option to start new cluster in kademlia check

--- cmd/beekeeper/cmd/check_kademlia.go
+++ cmd/beekeeper/cmd/check_kademlia.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/check/kademlia"
@@ -11,17 +12,27 @@ import (
 
 func (c *command) initCheckKademlia() *cobra.Command {
 	const (
-		optionNameDynamic       = "dynamic"
-		optionNameClusterName   = "cluster-name"
-		optionNameBootnodeCount = "bootnode-count"
-		optionNameNodeCount     = "node-count"
+		optionNameStartCluster   = "start-cluster"
+		optionNameDynamic        = "dynamic"
+		optionNameClusterName    = "cluster-name"
+		optionNameBootnodeCount  = "bootnode-count"
+		optionNameNodeCount      = "node-count"
+		optionNameImage          = "bee-image"
+		optionNamePersistence    = "persistence"
+		optionNameStorageClass   = "storage-class"
+		optionNameStorageRequest = "storage-request"
 	)
 
 	var (
-		dynamic       bool
-		clusterName   string
-		bootnodeCount int
-		nodeCount     int
+		startCluster   bool
+		dynamic        bool
+		clusterName    string
+		bootnodeCount  int
+		nodeCount      int
+		image          string
+		persistence    bool
+		storageClass   string
+		storageRequest string
 	)
 
 	cmd := &cobra.Command{
@@ -29,6 +40,11 @@ func (c *command) initCheckKademlia() *cobra.Command {
 		Short: "Checks Kademlia topology in the cluster",
 		Long:  `Checks Kademlia topology in the cluster.`,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			k8sClient, err := setK8SClient(c.config.GetString(optionNameKubeconfig), c.config.GetBool(optionNameInCluster))
+			if err != nil {
+				return fmt.Errorf("creating Kubernetes client: %v", err)
+			}
+
 			cluster := bee.NewCluster(clusterName, bee.ClusterOptions{
 				APIDomain:           c.config.GetString(optionNameAPIDomain),
 				APIInsecureTLS:      insecureTLSAPI,
@@ -36,32 +52,92 @@ func (c *command) initCheckKademlia() *cobra.Command {
 				DebugAPIDomain:      c.config.GetString(optionNameDebugAPIDomain),
 				DebugAPIInsecureTLS: insecureTLSDebugAPI,
 				DebugAPIScheme:      c.config.GetString(optionNameDebugAPIScheme),
+				K8SClient:           k8sClient,
 				Namespace:           c.config.GetString(optionNameNamespace),
 			})
 
-			if bootnodeCount > 0 {
+			if startCluster {
 				// bootnodes group
 				bgName := "bootnodes"
 				bgOptions := newDefaultNodeGroupOptions()
+				bgOptions.Image = image
+				bgOptions.Labels = map[string]string{
+					"app.kubernetes.io/component": "bootnode",
+					"app.kubernetes.io/part-of":   bgName,
+					"app.kubernetes.io/version":   strings.Split(image, ":")[1],
+				}
+				bgOptions.PersistenceEnabled = persistence
+				bgOptions.PersistenceStorageClass = storageClass
+				bgOptions.PersistanceStorageRequest = storageRequest
 				cluster.AddNodeGroup(bgName, *bgOptions)
 				bg := cluster.NodeGroup(bgName)
 
+				bSetup := setupBootnodes(bootnodeCount, c.config.GetString(optionNameNamespace))
 				for i := 0; i < bootnodeCount; i++ {
-					if err := bg.AddNode(fmt.Sprintf("bootnode-%d", i)); err != nil {
-						return fmt.Errorf("adding bootnode-%d: %v", i, err)
+					bConfig := newBeeDefaultConfig()
+					bConfig.Bootnodes = bSetup[i].Bootnodes
+					if err := bg.StartNode(cmd.Context(), bee.StartNodeOptions{
+						Name:         fmt.Sprintf("bootnode-%d", i),
+						Config:       *bConfig,
+						ClefKey:      bSetup[i].ClefKey,
+						ClefPassword: bSetup[i].ClefPassword,
+						LibP2PKey:    bSetup[i].LibP2PKey,
+						SwarmKey:     bSetup[i].SwarmKey,
+					}); err != nil {
+						return fmt.Errorf("starting bootnode-%d: %s", i, err)
 					}
 				}
-			}
 
-			// nodes group
-			ngName := "nodes"
-			ngOptions := newDefaultNodeGroupOptions()
-			cluster.AddNodeGroup(ngName, *ngOptions)
-			ng := cluster.NodeGroup(ngName)
+				// nodes group
+				ngName := "nodes"
+				ngOptions := newDefaultNodeGroupOptions()
+				ngOptions.Image = image
+				ngOptions.Labels = map[string]string{
+					"app.kubernetes.io/component": "node",
+					"app.kubernetes.io/part-of":   ngName,
+					"app.kubernetes.io/version":   strings.Split(image, ":")[1],
+				}
+				ngOptions.PersistenceEnabled = persistence
+				ngOptions.PersistenceStorageClass = storageClass
+				ngOptions.PersistanceStorageRequest = storageRequest
+				cluster.AddNodeGroup(ngName, *ngOptions)
+				ng := cluster.NodeGroup(ngName)
 
-			for i := 0; i < nodeCount; i++ {
-				if err := ng.AddNode(fmt.Sprintf("bee-%d", i)); err != nil {
-					return fmt.Errorf("adding bee-%d: %v", i, err)
+				nConfig := newBeeDefaultConfig()
+				nConfig.Bootnodes = setupBootnodesDNS(bootnodeCount, c.config.GetString(optionNameNamespace))
+				for i := 0; i < nodeCount; i++ {
+					if err := ng.StartNode(cmd.Context(), bee.StartNodeOptions{
+						Name:   fmt.Sprintf("bee-%d", i),
+						Config: *nConfig,
+					}); err != nil {
+						return fmt.Errorf("starting bee-%d: %s", i, err)
+					}
+				}
+			} else {
+				if bootnodeCount > 0 {
+					// bootnodes group
+					bgName := "bootnodes"
+					bgOptions := newDefaultNodeGroupOptions()
+					cluster.AddNodeGroup(bgName, *bgOptions)
+					bg := cluster.NodeGroup(bgName)
+
+					for i := 0; i < bootnodeCount; i++ {
+						if err := bg.AddNode(fmt.Sprintf("bootnode-%d", i)); err != nil {
+							return fmt.Errorf("adding bootnode-%d: %v", i, err)
+						}
+					}
+				}
+
+				// nodes group
+				ngName := "nodes"
+				ngOptions := newDefaultNodeGroupOptions()
+				cluster.AddNodeGroup(ngName, *ngOptions)
+				ng := cluster.NodeGroup(ngName)
+
+				for i := 0; i < nodeCount; i++ {
+					if err := ng.AddNode(fmt.Sprintf("bee-%d", i)); err != nil {
+						return fmt.Errorf("adding bee-%d: %v", i, err)
+					}
 				}
 			}
 
@@ -74,10 +150,15 @@ func (c *command) initCheckKademlia() *cobra.Command {
 		PreRunE: c.checkPreRunE,
 	}
 
+	cmd.Flags().BoolVar(&startCluster, optionNameStartCluster, false, "start new cluster")
 	cmd.Flags().BoolVar(&dynamic, optionNameDynamic, false, "check on dynamic cluster")
 	cmd.Flags().StringVar(&clusterName, optionNameClusterName, "beekeeper", "cluster name")
 	cmd.Flags().IntVarP(&bootnodeCount, optionNameBootnodeCount, "b", 0, "number of bootnodes")
 	cmd.Flags().IntVarP(&nodeCount, optionNameNodeCount, "c", 1, "number of nodes")
+	cmd.Flags().StringVar(&image, optionNameImage, "ethersphere/bee:latest", "Bee Docker image")
+	cmd.PersistentFlags().BoolVar(&persistence, optionNamePersistence, false, "use persistent storage")
+	cmd.PersistentFlags().StringVar(&storageClass, optionNameStorageClass, "local-storage", "storage class name")
+	cmd.PersistentFlags().StringVar(&storageRequest, optionNameStorageRequest, "34Gi", "storage request")
 
 	return cmd
 }
