e626bc16ab0a4190fb1c7b8142f33ff54ec32cba
svetomir <svetomirsmiljkovic@gmail.com>
2021-02-10 18:26:49 +0100

    Make cluster alterations

--- cmd/beekeeper/cmd/check_ping.go
+++ cmd/beekeeper/cmd/check_ping.go
@@ -1,14 +1,42 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/check"
 	"github.com/ethersphere/beekeeper/pkg/random"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 func (c *command) initCheckPing() *cobra.Command {
 	const (
-		optionNameSeed = "seed"
+		optionNameStartCluster   = "start-cluster"
+		optionNameDynamic        = "dynamic"
+		optionNameClusterName    = "cluster-name"
+		optionNameBootnodeCount  = "bootnode-count"
+		optionNameNodeCount      = "node-count"
+		optionNameImage          = "bee-image"
+		optionNameSeed           = "seed"
+		optionNamePersistence    = "persistence"
+		optionNameStorageClass   = "storage-class"
+		optionNameStorageRequest = "storage-request"
+	)
+
+	var (
+		startCluster   bool
+		dynamic        bool
+		clusterName    string
+		bootnodeCount  int
+		nodeCount      int
+		image          string
+		persistence    bool
+		storageClass   string
+		storageRequest string
 	)
 
 	cmd := &cobra.Command{
@@ -16,6 +44,137 @@ func (c *command) initCheckPing() *cobra.Command {
 		Short: "",
 		Long:  ``,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			k8sClient, err := setK8SClient(c.config.GetString(optionNameKubeconfig), c.config.GetBool(optionNameInCluster))
+			if err != nil {
+				return fmt.Errorf("creating Kubernetes client: %w", err)
+			}
+
+			cluster := bee.NewCluster(clusterName, bee.ClusterOptions{
+				APIDomain:           c.config.GetString(optionNameAPIDomain),
+				APIInsecureTLS:      insecureTLSAPI,
+				APIScheme:           c.config.GetString(optionNameAPIScheme),
+				DebugAPIDomain:      c.config.GetString(optionNameDebugAPIDomain),
+				DebugAPIInsecureTLS: insecureTLSDebugAPI,
+				DebugAPIScheme:      c.config.GetString(optionNameDebugAPIScheme),
+				K8SClient:           k8sClient,
+				Namespace:           c.config.GetString(optionNameNamespace),
+				DisableNamespace:    disableNamespace,
+			})
+
+			if startCluster {
+				// bootnodes group
+				bgName := "bootnodes"
+				bgOptions := newDefaultNodeGroupOptions()
+				bgOptions.Image = image
+				bgOptions.Labels = map[string]string{
+					"app.kubernetes.io/component": "bootnode",
+					"app.kubernetes.io/part-of":   bgName,
+					"app.kubernetes.io/version":   strings.Split(image, ":")[1],
+				}
+				bgOptions.PersistenceEnabled = persistence
+				bgOptions.PersistenceStorageClass = storageClass
+				bgOptions.PersistanceStorageRequest = storageRequest
+				cluster.AddNodeGroup(bgName, *bgOptions)
+				bg := cluster.NodeGroup(bgName)
+				bSetup := setupBootnodes(bootnodeCount, c.config.GetString(optionNameNamespace))
+
+				bCtx, bCancel := context.WithTimeout(cmd.Context(), 10*time.Minute)
+				defer bCancel()
+				bnGroup := new(errgroup.Group)
+				for i := 0; i < bootnodeCount; i++ {
+					bConfig := newDefaultBeeConfig()
+					bConfig.Bootnodes = bSetup[i].Bootnodes
+					bName := fmt.Sprintf("bootnode-%d", i)
+					bOptions := bee.NodeOptions{
+						Config:       bConfig,
+						ClefKey:      bSetup[i].ClefKey,
+						ClefPassword: bSetup[i].ClefPassword,
+						LibP2PKey:    bSetup[i].LibP2PKey,
+						SwarmKey:     bSetup[i].SwarmKey,
+					}
+
+					bnGroup.Go(func() error {
+						return bg.AddStartNode(bCtx, bName, bOptions)
+					})
+				}
+
+				if err := bnGroup.Wait(); err != nil {
+					return fmt.Errorf("starting bootnodes: %w", err)
+				}
+				fmt.Println("bootnodes started")
+
+				// nodes group
+				ngName := "nodes"
+				ngOptions := newDefaultNodeGroupOptions()
+				ngOptions.Image = image
+				ngOptions.Labels = map[string]string{
+					"app.kubernetes.io/component": "node",
+					"app.kubernetes.io/part-of":   ngName,
+					"app.kubernetes.io/version":   strings.Split(image, ":")[1],
+				}
+				ngOptions.PersistenceEnabled = persistence
+				ngOptions.PersistenceStorageClass = storageClass
+				ngOptions.PersistanceStorageRequest = storageRequest
+				ngOptions.BeeConfig = newDefaultBeeConfig()
+				ngOptions.BeeConfig.Bootnodes = setupBootnodesDNS(bootnodeCount, c.config.GetString(optionNameNamespace))
+				cluster.AddNodeGroup(ngName, *ngOptions)
+				ng := cluster.NodeGroup(ngName)
+
+				nCtx, nCancel := context.WithTimeout(cmd.Context(), 10*time.Minute)
+				defer nCancel()
+				nGroup := new(errgroup.Group)
+				for i := 0; i < nodeCount; i++ {
+					nName := fmt.Sprintf("bee-%d", i)
+
+					nGroup.Go(func() error {
+						return ng.AddStartNode(nCtx, nName, bee.NodeOptions{})
+					})
+				}
+
+				if err := nGroup.Wait(); err != nil {
+					return fmt.Errorf("starting nodes: %w", err)
+				}
+				fmt.Println("nodes started")
+
+			} else {
+				if bootnodeCount > 0 {
+					// bootnodes group
+					bgName := "bootnodes"
+					bgOptions := newDefaultNodeGroupOptions()
+					cluster.AddNodeGroup(bgName, *bgOptions)
+					bg := cluster.NodeGroup(bgName)
+
+					for i := 0; i < bootnodeCount; i++ {
+						if err := bg.AddNode(fmt.Sprintf("bootnode-%d", i), bee.NodeOptions{}); err != nil {
+							return fmt.Errorf("adding bootnode-%d: %w", i, err)
+						}
+					}
+				}
+
+				// nodes group
+				ngName := "nodes"
+				ngOptions := newDefaultNodeGroupOptions()
+				ngOptions.Image = image
+				ngOptions.Labels = map[string]string{
+					"app.kubernetes.io/component": "node",
+					"app.kubernetes.io/part-of":   ngName,
+					"app.kubernetes.io/version":   strings.Split(image, ":")[1],
+				}
+				ngOptions.PersistenceEnabled = persistence
+				ngOptions.PersistenceStorageClass = storageClass
+				ngOptions.PersistanceStorageRequest = storageRequest
+				ngOptions.BeeConfig = newDefaultBeeConfig()
+				ngOptions.BeeConfig.Bootnodes = setupBootnodesDNS(bootnodeCount, c.config.GetString(optionNameNamespace))
+				cluster.AddNodeGroup(ngName, *ngOptions)
+				ng := cluster.NodeGroup(ngName)
+
+				for i := 0; i < nodeCount; i++ {
+					if err := ng.AddNode(fmt.Sprintf("bee-%d", i), bee.NodeOptions{}); err != nil {
+						return fmt.Errorf("adding bee-%d: %w", i, err)
+					}
+				}
+			}
+
 			var seed int64
 			if cmd.Flags().Changed("seed") {
 				seed = c.config.GetInt64(optionNameSeed)
@@ -24,14 +183,68 @@ func (c *command) initCheckPing() *cobra.Command {
 			}
 
 			runOptions := check.RunOptions{
-				Seed: seed,
+				Cluster: cluster,
+				Seed:    seed,
+				Stages:  runStages,
 			}
+
 			return check.Run(cmd.Context(), runOptions)
 		},
 		PreRunE: c.checkPreRunE,
 	}
 
 	cmd.Flags().Int64P(optionNameSeed, "s", 0, "seed for generating chunks; if not set, will be random")
+	cmd.Flags().BoolVar(&startCluster, optionNameStartCluster, false, "start new cluster")
+	cmd.Flags().BoolVar(&dynamic, optionNameDynamic, false, "check on dynamic cluster")
+	cmd.Flags().StringVar(&clusterName, optionNameClusterName, "beekeeper", "cluster name")
+	cmd.Flags().IntVarP(&bootnodeCount, optionNameBootnodeCount, "b", 0, "number of bootnodes")
+	cmd.Flags().IntVarP(&nodeCount, optionNameNodeCount, "c", 1, "number of nodes")
+	cmd.Flags().StringVar(&image, optionNameImage, "ethersphere/bee:latest", "Bee Docker image")
+	cmd.PersistentFlags().BoolVar(&persistence, optionNamePersistence, false, "use persistent storage")
+	cmd.PersistentFlags().StringVar(&storageClass, optionNameStorageClass, "local-storage", "storage class name")
+	cmd.PersistentFlags().StringVar(&storageRequest, optionNameStorageRequest, "34Gi", "storage request")
 
 	return cmd
+}
+
+var runStages = []check.Stage{
+	{
+		Updates: []check.Update{
+			{
+				NodeGroup: "nodes",
+				Actions: check.Actions{
+					AddCount:    1,
+					StartCount:  0,
+					StopCount:   1,
+					DeleteCount: 1,
+				},
+			},
+		},
+	},
+	{
+		Updates: []check.Update{
+			{
+				NodeGroup: "nodes",
+				Actions: check.Actions{
+					AddCount:    2,
+					StartCount:  1,
+					StopCount:   2,
+					DeleteCount: 1,
+				},
+			},
+		},
+	},
+	{
+		Updates: []check.Update{
+			{
+				NodeGroup: "nodes",
+				Actions: check.Actions{
+					AddCount:    3,
+					StartCount:  1,
+					StopCount:   2,
+					DeleteCount: 1,
+				},
+			},
+		},
+	},
 }
--- pkg/check/check.go
+++ pkg/check/check.go
@@ -3,14 +3,25 @@ package check
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/ethersphere/beekeeper/pkg/bee"
+	"github.com/ethersphere/beekeeper/pkg/random"
 )
+
+// Options ...
+type Options struct{}
+
+// Check ...
+type Check interface {
+	Run(ctx context.Context, o Options) (err error)
+}
 
 // RunOptions for updating cluster
 type RunOptions struct {
-	Seed   int64
-	Stages []Stage
+	Cluster *bee.Cluster
+	Seed    int64
+	Stages  []Stage
 }
 
 // Stage ...
@@ -20,7 +31,6 @@ type Stage struct {
 
 // Update represents details for updating a node group
 type Update struct {
-	Seed      int64
 	NodeGroup string
 	Actions   Actions
 }
@@ -36,11 +46,16 @@ type Actions struct {
 // Run runs check against the cluster
 func Run(ctx context.Context, o RunOptions) (err error) {
 	fmt.Printf("root seed: %d\n", o.Seed)
-	// rnds := random.PseudoGenerators(o.Seed, len(o.Updates))
 
 	for i, stage := range o.Stages {
 		for j, update := range stage.Updates {
-			fmt.Println(i, stage, j, update)
+			fmt.Println("stage", i, "update", j, update.NodeGroup, update.Actions)
+
+			rnd := random.PseudoGenerator(o.Seed)
+			ng := o.Cluster.NodeGroup(update.NodeGroup)
+			if err := updateNodeGroup(ctx, i, rnd, ng, update.Actions); err != nil {
+				return err
+			}
 		}
 
 		// run check here
@@ -49,78 +64,69 @@ func Run(ctx context.Context, o RunOptions) (err error) {
 	return
 }
 
-func updateNodeGroup(ctx context.Context, ng *bee.NodeGroup, a Actions) (err error) {
+func updateNodeGroup(ctx context.Context, i int, rnd *rand.Rand, ng *bee.NodeGroup, a Actions) (err error) {
+	// delete nodes
+	for j := 0; j < a.DeleteCount; j++ {
+		nName := ng.NodesSorted()[rnd.Intn(ng.Size())]
+		overlay, err := ng.NodeClient(nName).Overlay(ctx)
+		if err != nil {
+			return fmt.Errorf("get node %s overlay: %w", nName, err)
+		}
+		if err := ng.DeleteNode(ctx, nName); err != nil {
+			return fmt.Errorf("delete node %s: %w", nName, err)
+		}
+		fmt.Printf("node %s (%s) is deleted\n", nName, overlay)
+	}
+
+	// start nodes
+	for j := 0; j < a.StartCount; j++ {
+		stopped, err := ng.StoppedNodes(ctx)
+		if err != nil {
+			return fmt.Errorf("stoped nodes: %w", err)
+		}
+		if len(stopped) > 0 {
+			nName := stopped[rnd.Intn(len(stopped))]
+			if err := ng.StartNode(ctx, nName); err != nil {
+				return fmt.Errorf("start node %s: %w", nName, err)
+			}
+			overlay, err := ng.NodeClient(nName).Overlay(ctx)
+			if err != nil {
+				return fmt.Errorf("get node %s overlay: %w", nName, err)
+			}
+			fmt.Printf("node %s (%s) is started\n", nName, overlay)
+		}
+	}
+
+	// stop nodes
+	for j := 0; j < a.StopCount; j++ {
+		started, err := ng.StartedNodes(ctx)
+		if err != nil {
+			return fmt.Errorf("started nodes: %w", err)
+		}
+		if len(started) > 0 {
+			nName := started[rnd.Intn(len(started))]
+			overlay, err := ng.NodeClient(nName).Overlay(ctx)
+			if err != nil {
+				return fmt.Errorf("get node %s overlay: %w", nName, err)
+			}
+			if err := ng.StopNode(ctx, nName); err != nil {
+				return fmt.Errorf("stop node %s: %w", nName, err)
+			}
+			fmt.Printf("node %s (%s) is stopped\n", nName, overlay)
+		}
+	}
+
+	// add nodes
+	for j := 0; j < a.AddCount; j++ {
+		nName := fmt.Sprintf("bee-i%dn%d", i, j)
+		if err := ng.AddStartNode(ctx, nName, bee.NodeOptions{}); err != nil {
+			return fmt.Errorf("add start node %s: %w", nName, err)
+		}
+		overlay, err := ng.NodeClient(nName).Overlay(ctx)
+		if err != nil {
+			return fmt.Errorf("get node %s overlay: %w", nName, err)
+		}
+		fmt.Printf("node %s (%s) is added\n", nName, overlay)
+	}
 	return
-}
-
-// // delete nodes
-// for j := 0; j < a.DeleteCount; j++ {
-// 	nName := ng.NodesSorted()[rnd.Intn(ng.Size())]
-// 	overlay, err := ng.NodeClient(nName).Overlay(ctx)
-// 	if err != nil {
-// 		return fmt.Errorf("get node %s overlay: %w", nName, err)
-// 	}
-// 	if err := ng.DeleteNode(ctx, nName); err != nil {
-// 		return fmt.Errorf("delete node %s: %w", nName, err)
-// 	}
-// 	fmt.Printf("node %s (%s) is deleted\n", nName, overlay)
-// }
-
-// // start nodes
-// for j := 0; j < a.StartCount; j++ {
-// 	stopped, err := ng.StoppedNodes(ctx)
-// 	if err != nil {
-// 		return fmt.Errorf("stoped nodes: %w", err)
-// 	}
-// 	if len(stopped) > 0 {
-// 		nName := stopped[rnd.Intn(len(stopped))]
-// 		if err := ng.StartNode(ctx, nName); err != nil {
-// 			return fmt.Errorf("start node %s: %w", nName, err)
-// 		}
-// 		overlay, err := ng.NodeClient(nName).Overlay(ctx)
-// 		if err != nil {
-// 			return fmt.Errorf("get node %s overlay: %w", nName, err)
-// 		}
-// 		fmt.Printf("node %s (%s) is started\n", nName, overlay)
-// 	}
-// }
-
-// // stop nodes
-// for j := 0; j < a.StopCount; j++ {
-// 	started, err := ng.StartedNodes(ctx)
-// 	if err != nil {
-// 		return fmt.Errorf("started nodes: %w", err)
-// 	}
-// 	if len(started) > 0 {
-// 		nName := started[rnd.Intn(len(started))]
-// 		overlay, err := ng.NodeClient(nName).Overlay(ctx)
-// 		if err != nil {
-// 			return fmt.Errorf("get node %s overlay: %w", nName, err)
-// 		}
-// 		if err := ng.StopNode(ctx, nName); err != nil {
-// 			return fmt.Errorf("stop node %s: %w", nName, err)
-// 		}
-// 		fmt.Printf("node %s (%s) is stopped\n", nName, overlay)
-// 	}
-// }
-
-// // add nodes
-// for j := 0; j < a.AddCount; j++ {
-// 	nName := fmt.Sprintf("bee-i%dn%d", i, j)
-// 	if err := ng.AddStartNode(ctx, nName, bee.NodeOptions{}); err != nil {
-// 		return fmt.Errorf("add start node %s: %w", nName, err)
-// 	}
-// 	overlay, err := ng.NodeClient(nName).Overlay(ctx)
-// 	if err != nil {
-// 		return fmt.Errorf("get node %s overlay: %w", nName, err)
-// 	}
-// 	fmt.Printf("node %s (%s) is added\n", nName, overlay)
-// }
-
-// Options ...
-type Options struct{}
-
-// Check ...
-type Check interface {
-	Run(ctx context.Context, o Options) (err error)
 }
