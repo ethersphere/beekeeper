d6b9c97956578633e1e5bf3defe2128de9e53957
Esad Akar <esadakar@gmail.com>
2021-04-06 20:10:25 +0300

    pushsync replication loop fix (#123)
    * pushsync replication loop fix
    
    * pushsync: added uploader address to skip peers list

--- pkg/check/pushsync/check_chunks.go
+++ pkg/check/pushsync/check_chunks.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ethersphere/bee/pkg/swarm"
 	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/beeclient/api"
 	"github.com/ethersphere/beekeeper/pkg/random"
@@ -25,13 +26,16 @@ func CheckChunks(c *bee.Cluster, o Options) (err error) {
 	sortedNodes := ng.NodesSorted()
 	for i := 0; i < o.UploadNodeCount; i++ {
 		nodeName := sortedNodes[i]
+	testCases:
 		for j := 0; j < o.ChunksPerNode; j++ {
 			chunk, err := bee.NewRandomChunk(rnds[i])
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
 
-			ref, err := ng.NodeClient(nodeName).UploadChunk(ctx, chunk.Data(), api.UploadOptions{Pin: false})
+			uploader := ng.NodeClient(nodeName)
+
+			ref, err := uploader.UploadChunk(ctx, chunk.Data(), api.UploadOptions{Pin: false})
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
@@ -41,7 +45,7 @@ func CheckChunks(c *bee.Cluster, o Options) (err error) {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
 
-			time.Sleep(1 * time.Second)
+			time.Sleep(2 * time.Second)
 			synced, err := ng.NodeClient(closestName).HasChunk(ctx, ref)
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
@@ -53,22 +57,31 @@ func CheckChunks(c *bee.Cluster, o Options) (err error) {
 
 			fmt.Printf("Node %s. Chunk %d found in the closest node %s Chunk: %s\n", nodeName, j, closestAddress.String(), ref.String())
 
-			// chunk should be replicated at least once in the neighborhood
-			closestName, closestAddress, err = chunk.ClosestNodeFromMap(overlays, closestAddress)
+			uploaderAddr, err := uploader.Overlay(ctx)
 			if err != nil {
-				return fmt.Errorf("node %s: %w", nodeName, err)
-			}
-			synced, err = ng.NodeClient(closestName).HasChunk(ctx, ref)
-			if err != nil {
-				return fmt.Errorf("node %s: %w", nodeName, err)
-			}
-			if !synced {
-				fmt.Printf("Node %s. Chunk %d NOT found in the second closest node %s Chunk: %s\n", nodeName, j, closestAddress.String(), ref.String())
-				return errPushSync
+				return err
 			}
 
-			fmt.Printf("Node %s. Chunk %d found in the second closest node %s Chunk: %s\n", nodeName, j, closestAddress.String(), ref.String())
+			skipPeers := []swarm.Address{closestAddress, uploaderAddr}
+			// chunk should be replicated at least once either during forwarding or after storing
+			for range overlays {
+				name, address, err := chunk.ClosestNodeFromMap(overlays, skipPeers...)
+				skipPeers = append(skipPeers, address)
+				if err != nil {
+					continue
+				}
+				synced, err = ng.NodeClient(name).HasChunk(ctx, ref)
+				if err != nil {
+					continue
+				}
+				if synced {
+					fmt.Printf("Node %s. Chunk %d was replicated to node %s Chunk: %s\n", name, j, address.String(), ref.String())
+					continue testCases
+				}
+			}
 
+			fmt.Printf("Node %s. Chunk %d NOT replicated to a node\n", nodeName, j)
+			return errPushSync
 		}
 	}
 
