557e5d4d8a960897fdfd4b7976d761197a25bd18
Svetomir Smiljkovic <svetomir@ethswarm.org>
2021-04-26 16:13:02 +0200

    Support multi node-groups in gc check (#136)
    * Support multi node-groups in gc check
    
    * Return only running random node

--- pkg/bee/cluster.go
+++ pkg/bee/cluster.go
@@ -268,6 +268,26 @@ func (c *Cluster) Peers(ctx context.Context) (peers ClusterPeers, err error) {
 	return
 }
 
+// RandomNode returns random running node from a cluster
+func (c *Cluster) RandomNode(ctx context.Context, r *rand.Rand) (node *Node, err error) {
+	nodes := []*Node{}
+	for _, ng := range c.NodeGroups() {
+		stopped, err := ng.StoppedNodes(ctx)
+		if err != nil && err != k8s.ErrNotSet {
+			return nil, fmt.Errorf("stopped nodes: %w", err)
+		}
+
+		for _, v := range ng.getNodes() {
+			if contains(stopped, v.name) {
+				continue
+			}
+			nodes = append(nodes, v)
+		}
+	}
+
+	return nodes[r.Intn(len(nodes))], nil
+}
+
 // ClusterSettlements represents settlements of all nodes in the cluster
 type ClusterSettlements map[string]NodeGroupSettlements
 
