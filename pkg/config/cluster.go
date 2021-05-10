510aa1dfe6c38a2bd627c0afd47e86673775d48e
svetomir <svetomirsmiljkovic@gmail.com>
2021-05-10 13:36:08 +0200

    Update config types

--- pkg/config/cluster.go
+++ pkg/config/cluster.go
@@ -11,32 +11,35 @@ type Cluster struct {
 	// parent to inherit settings from
 	*Inherit `yaml:",inline"`
 	// Cluster configuration
-	Name                *string           `yaml:"name"`
-	Namespace           *string           `yaml:"namespace"`
-	DisableNamespace    *bool             `yaml:"disable-namespace"`
-	APIDomain           *string           `yaml:"api-domain"`
-	APIInsecureTLS      *bool             `yaml:"api-insecure-tls"`
-	APIScheme           *string           `yaml:"api-scheme"`
-	DebugAPIDomain      *string           `yaml:"debug-api-domain"`
-	DebugAPIInsecureTLS *bool             `yaml:"debug-api-insecure-tls"`
-	DebugAPIScheme      *string           `yaml:"debug-api-scheme"`
-	NodeGroups          *ClusterNodeGroup `yaml:"node-groups"`
+	Name                *string                      `yaml:"name"`
+	Namespace           *string                      `yaml:"namespace"`
+	DisableNamespace    *bool                        `yaml:"disable-namespace"`
+	APIDomain           *string                      `yaml:"api-domain"`
+	APIInsecureTLS      *bool                        `yaml:"api-insecure-tls"`
+	APIScheme           *string                      `yaml:"api-scheme"`
+	DebugAPIDomain      *string                      `yaml:"debug-api-domain"`
+	DebugAPIInsecureTLS *bool                        `yaml:"debug-api-insecure-tls"`
+	DebugAPIScheme      *string                      `yaml:"debug-api-scheme"`
+	NodeGroups          *map[string]ClusterNodeGroup `yaml:"node-groups"`
 }
 
-// ClusterNodeGroup represents NodeGroup in the Cluster
-type ClusterNodeGroup map[string]struct {
-	Mode      string `yaml:"mode"`
-	BeeConfig string `yaml:"bee-config"`
-	Config    string `yaml:"config"`
-	Count     int    `yaml:"count"`
-	Nodes     []struct {
-		Name         string `yaml:"name"`
-		Bootnodes    string `yaml:"bootnodes"`
-		ClefKey      string `yaml:"clef-key"`
-		ClefPassword string `yaml:"clef-password"`
-		LibP2PKey    string `yaml:"libp2p-key"`
-		SwarmKey     string `yaml:"swarm-key"`
-	} `yaml:"nodes"`
+// ClusterNodeGroup represents node group in the cluster
+type ClusterNodeGroup struct {
+	Mode      string        `yaml:"mode"`
+	BeeConfig string        `yaml:"bee-config"`
+	Config    string        `yaml:"config"`
+	Count     int           `yaml:"count"`
+	Nodes     []ClusterNode `yaml:"nodes"`
+}
+
+// ClusterNode represents node in the cluster
+type ClusterNode struct {
+	Name         string `yaml:"name"`
+	Bootnodes    string `yaml:"bootnodes"`
+	ClefKey      string `yaml:"clef-key"`
+	ClefPassword string `yaml:"clef-password"`
+	LibP2PKey    string `yaml:"libp2p-key"`
+	SwarmKey     string `yaml:"swarm-key"`
 }
 
 // Export exports Cluster to bee.ClusterOptions, skipping all other extra fields
@@ -78,7 +81,7 @@ func (c *Cluster) GetNamespace() string {
 }
 
 // GetNodeGroups returns cluster node groups
-func (c *Cluster) GetNodeGroups() ClusterNodeGroup {
+func (c *Cluster) GetNodeGroups() map[string]ClusterNodeGroup {
 	if c.NodeGroups == nil {
 		return nil
 	}
