fea8e39dae445be61569fd7cc9448b9038c60ba7
svetomir <svetomirsmiljkovic@gmail.com>
2021-05-20 13:43:15 +0200

    Update ingress to new k8s api

--- pkg/k8s/ingress/tls.go
+++ pkg/k8s/ingress/tls.go
@@ -1,13 +1,13 @@
 package ingress
 
-import ev1b1 "k8s.io/api/extensions/v1beta1"
+import v1 "k8s.io/api/networking/v1"
 
 // TLSs represents service's IngressTLSs
 type TLSs []TLS
 
 // toK8S converts TLSs to Kuberntes client objects
-func (ts TLSs) toK8S() (l []ev1b1.IngressTLS) {
-	l = make([]ev1b1.IngressTLS, 0, len(ts))
+func (ts TLSs) toK8S() (l []v1.IngressTLS) {
+	l = make([]v1.IngressTLS, 0, len(ts))
 
 	for _, t := range ts {
 		l = append(l, t.toK8S())
@@ -23,8 +23,8 @@ type TLS struct {
 }
 
 // toK8S converts TLS to Kuberntes client object
-func (t *TLS) toK8S() (tls ev1b1.IngressTLS) {
-	return ev1b1.IngressTLS{
+func (t *TLS) toK8S() (tls v1.IngressTLS) {
+	return v1.IngressTLS{
 		Hosts: func() (hosts []string) {
 			hosts = append(hosts, t.Hosts...)
 			return
