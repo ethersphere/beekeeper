bf9ae909631cfcba9e97db7e7e907a05d255078f
svetomir <svetomirsmiljkovic@gmail.com>
2021-09-21 15:08:18 +0200

    Resolve conflict again

--- pkg/check/postage/postage.go
+++ pkg/check/postage/postage.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	mbig "math/big"
 
-	"github.com/ethersphere/beekeeper/pkg/bee"
 	"github.com/ethersphere/beekeeper/pkg/beekeeper"
+	"github.com/ethersphere/beekeeper/pkg/orchestration"
 )
 
 // Options represents check options
@@ -44,7 +44,7 @@ func NewCheck() beekeeper.Action {
 	return &Check{}
 }
 
-func (c *Check) Run(ctx context.Context, cluster *bee.Cluster, opts interface{}) (err error) {
+func (c *Check) Run(ctx context.Context, cluster orchestration.Cluster, opts interface{}) (err error) {
 	o, ok := opts.(Options)
 	if !ok {
 		return fmt.Errorf("invalid options type")
