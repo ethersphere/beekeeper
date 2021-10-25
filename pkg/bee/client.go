5a9b38e40bcd8330f0f9949091160582197ded33
Anatolie Lupacescu <anatol@ethswarm.org>
2021-10-25 11:29:13 +0300

    fix: compilation err

--- pkg/bee/client.go
+++ pkg/bee/client.go
@@ -748,11 +748,11 @@ func (c *Client) Reupload(ctx context.Context, ref swarm.Address) error {
 // Authenticate
 func (c *Client) Authenticate(ctx context.Context, role, password string) (string, error) {
 	resp, err := c.api.Auth.Authenticate(ctx, role, password)
-	return resp.Key, err
+	return resp, err
 }
 
 // Refresh
 func (c *Client) Refresh(ctx context.Context, securityToken string) (string, error) {
 	resp, err := c.api.Auth.Refresh(ctx, securityToken)
-	return resp.Key, err
+	return resp, err
 }
