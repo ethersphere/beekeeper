0552a70ec60442322fcb861ded76c84c3d9c9797
mrekucci <peter@ethswarm.org>
2021-11-30 16:42:43 +0100

    fead: disable deferred upload on smoke test

--- pkg/check/smoke/smoke.go
+++ pkg/check/smoke/smoke.go
@@ -177,7 +177,7 @@ func (t *test) upload(cName string, data []byte) (swarm.Address, time.Duration, 
 
 	fmt.Printf("node %s: uploading data\n", cName)
 	start := time.Now()
-	addr, err := client.UploadBytes(t.ctx, data, api.UploadOptions{Pin: false, BatchID: batchID})
+	addr, err := client.UploadBytes(t.ctx, data, api.UploadOptions{Pin: false, BatchID: batchID, Deferred: false})
 	if err != nil {
 		return swarm.ZeroAddress, 0, fmt.Errorf("upload to the node %s: %w", cName, err)
 	}
