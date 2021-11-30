594804e9199440ca78b347f72c5e323159958340
istae <14264581+istae@users.noreply.github.com>
2021-11-30 13:58:49 +0300

    lightnode fix (#219)

--- pkg/check/pushsync/check_lightnode.go
+++ pkg/check/pushsync/check_lightnode.go
@@ -14,6 +14,7 @@ import (
 
 // checkChunks uploads given chunks on cluster and checks pushsync ability of the cluster
 func checkLightChunks(ctx context.Context, cluster orchestration.Cluster, o Options) error {
+
 	rnds := random.PseudoGenerators(o.Seed, o.UploadNodeCount)
 	fmt.Printf("seed: %d\n", o.Seed)
 
@@ -28,23 +29,24 @@ func checkLightChunks(ctx context.Context, cluster orchestration.Cluster, o Opti
 	}
 
 	for i, nodeName := range cluster.LightNodeNames() {
+
 		if i >= o.UploadNodeCount {
 			break
 		}
+
+		uploader := clients[nodeName]
+		batchID, err := uploader.GetOrCreateBatch(ctx, o.PostageAmount, o.PostageDepth, o.GasPrice, o.PostageLabel)
+		if err != nil {
+			return fmt.Errorf("node %s: batch id %w", nodeName, err)
+		}
+		fmt.Printf("node %s: batch id %s\n", nodeName, batchID)
+
 	testCases:
 		for j := 0; j < o.ChunksPerNode; j++ {
 			chunk, err := bee.NewRandomChunk(rnds[i])
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
-
-			uploader := clients[nodeName]
-
-			batchID, err := uploader.GetOrCreateBatch(ctx, o.PostageAmount, o.PostageDepth, o.GasPrice, o.PostageLabel)
-			if err != nil {
-				return fmt.Errorf("node %s: batch id %w", nodeName, err)
-			}
-			fmt.Printf("node %s: batch id %s\n", nodeName, batchID)
 
 			var ref swarm.Address
 
@@ -53,27 +55,29 @@ func checkLightChunks(ctx context.Context, cluster orchestration.Cluster, o Opti
 				if err == nil {
 					break
 				}
-				time.Sleep(5 * time.Second)
+				time.Sleep(o.RetryDelay)
 			}
-
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
 
 			fmt.Printf("uploaded chunk %s to node %s\n", ref.String(), nodeName)
+
+			time.Sleep(o.RetryDelay)
+
 			closestName, closestAddress, err := chunk.ClosestNodeFromMap(overlays)
 			if err != nil {
 				return fmt.Errorf("node %s: %w", nodeName, err)
 			}
 			fmt.Printf("closest node %s overlay %s\n", closestName, closestAddress)
 
-			time.Sleep(o.RetryDelay)
-
-			node := clients[closestName]
-
-			synced, err := node.HasChunk(ctx, ref)
-			if err != nil {
-				return fmt.Errorf("node %s: %w", nodeName, err)
+			var synced bool
+			for i := 0; i < 3; i++ {
+				synced, _ = clients[closestName].HasChunk(ctx, ref)
+				if synced {
+					break
+				}
+				time.Sleep(o.RetryDelay)
 			}
 			if !synced {
 				return fmt.Errorf("node %s chunk %s not found in the closest node %s", nodeName, ref.String(), closestAddress)
