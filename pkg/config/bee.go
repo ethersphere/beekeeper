b7b97edd7a190b531e6ff32c2bd1929a2b2fb96f
istae <14264581+istae@users.noreply.github.com>
2022-03-29 19:17:48 +0300

    fix: mainnet config fix

--- pkg/config/bee.go
+++ pkg/config/bee.go
@@ -32,6 +32,7 @@ type BeeConfig struct {
 	GatewayMode                *bool          `yaml:"gateway-mode"`
 	GlobalPinningEnabled       *bool          `yaml:"global-pinning-enabled"`
 	NATAddr                    *string        `yaml:"nat-addr"`
+	Mainnet                    *bool          `yaml:"mainnet"`
 	NetworkID                  *uint64        `yaml:"network-id"`
 	P2PAddr                    *string        `yaml:"p2p-addr"`
 	P2PQUICEnable              *bool          `yaml:"p2p-quic-enable"`
