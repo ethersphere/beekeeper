19062525d85a7befe90df338050b902b928bba26
istae <14264581+istae@users.noreply.github.com>
2022-04-27 16:39:33 +0300

    chore: remove global pinning config (#251)

--- pkg/config/bee.go
+++ pkg/config/bee.go
@@ -30,7 +30,6 @@ type BeeConfig struct {
 	DebugAPIEnable             *bool          `yaml:"debug-api-enable"`
 	FullNode                   *bool          `yaml:"full-node"`
 	GatewayMode                *bool          `yaml:"gateway-mode"`
-	GlobalPinningEnabled       *bool          `yaml:"global-pinning-enabled"`
 	NATAddr                    *string        `yaml:"nat-addr"`
 	Mainnet                    *bool          `yaml:"mainnet"`
 	NetworkID                  *uint64        `yaml:"network-id"`
