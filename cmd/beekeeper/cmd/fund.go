1cd5c5b970ddec97185d9b541f5e57fb2d8d71ba
Svetomir Smiljkovic <svetomirsmiljkovic@gmail.com>
2022-05-30 10:00:43 +0200

    Update printing of keys (#260)
    * Update printing of keys
    
    * fix: add 2 new flags and change printed output
    
    * fix: update linter
    
    * feat: print funded info into stderr
    
    * fix: hardcode go 1.18
    
    * fix: hardcode go 1.17
    
    * fix: skip-cache true for golinter
    
    * Fix lint
    
    Co-authored-by: Ivan Vandot <ivan@vandot.rs>

--- cmd/beekeeper/cmd/fund.go
+++ cmd/beekeeper/cmd/fund.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/ethersphere/beekeeper/pkg/orchestration/utils"
@@ -22,6 +23,8 @@ func (c *command) initFundCmd() (err error) {
 		optionNameEthDeposit      = "eth-deposit"
 		optionNameGBzzDeposit     = "gBzz-deposit"
 		optionNamePassword        = "password"
+		optionNamePrintKeys       = "print-keys"
+		optionNamePrintAddresses  = "print-addresses"
 		optionNameTimeout         = "timeout"
 	)
 
@@ -33,6 +36,7 @@ beekeeper fund --addresses=0xf176839c150e52fe30e5c2b5c648465c6fdfa532,0xebe269e0
 beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-deposit 0.1`,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			var addresses []string
+			var createdKeys []utils.EncryptedKey
 			if c.globalConfig.GetBool(optionNameAddressCreate) {
 				for i := 0; i < c.globalConfig.GetInt(optionNameAddressCount); i++ {
 					swarmKey, err := utils.CreateSwarmKey(c.globalConfig.GetString(optionNamePassword))
@@ -40,11 +44,19 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 						return fmt.Errorf("creating Swarm key: %w", err)
 					}
 
-					var key struct{ Address string }
+					var key utils.EncryptedKey
 					if err := json.Unmarshal([]byte(swarmKey), &key); err != nil {
 						return fmt.Errorf("unmarshaling Swarm address: %w", err)
 					}
 					addresses = append(addresses, "0x"+key.Address)
+					createdKeys = append(createdKeys, key)
+				}
+				if c.globalConfig.GetBool(optionNamePrintKeys) {
+					k, err := json.Marshal(createdKeys)
+					if err != nil {
+						return fmt.Errorf("marshaling Swarm keys: %w", err)
+					}
+					fmt.Printf("%s\n", k)
 				}
 			} else if len(c.globalConfig.GetStringSlice(optionNameAddresses)) < 1 {
 				return fmt.Errorf("bee node Ethereum addresses not provided")
@@ -56,7 +68,9 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 			defer cancel()
 
 			for _, a := range addresses {
-				fmt.Printf("address: %s\n", a)
+				if c.globalConfig.GetBool(optionNamePrintAddresses) {
+					fmt.Printf("address: %s\n", a)
+				}
 				// ETH funding
 				ethDeposit := c.globalConfig.GetFloat64(optionNameEthDeposit)
 				if ethDeposit > 0 {
@@ -64,7 +78,7 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 					if err != nil {
 						return fmt.Errorf("send eth: %w", err)
 					}
-					fmt.Printf("%s funded with %.2f ETH, transaction: %s\n", a, ethDeposit, tx)
+					fmt.Fprintf(os.Stderr, "%s funded with %.2f ETH, transaction: %s\n", a, ethDeposit, tx)
 				}
 				// BZZ funding
 				bzzDeposit := c.globalConfig.GetFloat64(optionNameBzzDeposit)
@@ -73,7 +87,7 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 					if err != nil {
 						return fmt.Errorf("deposit bzz: %w", err)
 					}
-					fmt.Printf("%s funded with %.2f BZZ, transaction: %s\n", a, bzzDeposit, tx)
+					fmt.Fprintf(os.Stderr, "%s funded with %.2f BZZ, transaction: %s\n", a, bzzDeposit, tx)
 				}
 				// gBZZ funding
 				gBzzDeposit := c.globalConfig.GetFloat64(optionNameGBzzDeposit)
@@ -82,7 +96,7 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 					if err != nil {
 						return fmt.Errorf("deposit gBzz: %w", err)
 					}
-					fmt.Printf("%s funded with %.2f gBZZ, transaction: %s\n", a, gBzzDeposit, tx)
+					fmt.Fprintf(os.Stderr, "%s funded with %.2f gBZZ, transaction: %s\n", a, gBzzDeposit, tx)
 				}
 			}
 
@@ -101,6 +115,8 @@ beekeeper fund --address-create --address-count 2 --bzz-deposit 100.0 --eth-depo
 	cmd.Flags().Float64(optionNameGBzzDeposit, 0, "gBZZ tokens amount to deposit")
 	cmd.Flags().Float64(optionNameEthDeposit, 0, "ETH amount to deposit")
 	cmd.Flags().String(optionNamePassword, "beekeeper", "password for generating Ethereum addresses")
+	cmd.Flags().Bool(optionNamePrintKeys, false, "if enabled, prints created keys")
+	cmd.Flags().Bool(optionNamePrintAddresses, false, "if enabled, prints funded addresses")
 	cmd.Flags().Duration(optionNameTimeout, 5*time.Minute, "timeout")
 
 	c.root.AddCommand(cmd)
