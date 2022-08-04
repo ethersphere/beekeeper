6e1daa0a1e2839e732a908c498f9d0af954b0d60
Ljubisa Gacevic <35105035+gacevicljubisa@users.noreply.github.com>
2022-08-04 15:27:39 +0200

    chore: change version to 0.11.3 (#277)

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package beekeeper
 
 var (
-	version = "0.11.2" // manually set semantic version number
+	version = "0.11.3" // manually set semantic version number
 	commit  string     // automatically set git commit hash
 
 	// Version TODO
