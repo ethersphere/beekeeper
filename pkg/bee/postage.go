54c120486919c299e621cf6f77efe1625a41dead
Ljubisa <ljubisa.rs@gmail.com>
2022-09-12 14:56:39 +0200

    fix(postage): MinimumBatchDepth changed to 16

--- pkg/bee/postage.go
+++ pkg/bee/postage.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
-const MinimumBatchDepth = 2
+const MinimumBatchDepth = 16
 
 func EstimatePostageBatchDepth(contentLength int64) uint64 {
 	depth := uint64(math.Log2(float64(calculateNumberOfChunks(contentLength, false))))
