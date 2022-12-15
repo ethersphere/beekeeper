80b641aee7bc45509d8a7927d0b233b8a7dc312c
Ljubisa Gacevic <35105035+gacevicljubisa@users.noreply.github.com>
2022-12-15 16:52:04 +0100

    chore: bump version to 0.11.11 (#305)

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package beekeeper
 
 var (
-	version = "0.11.7" // manually set semantic version number
-	commit  string     // automatically set git commit hash
+	version = "0.11.11" // manually set semantic version number
+	commit  string      // automatically set git commit hash
 
 	// Version TODO
 	Version = func() string {
