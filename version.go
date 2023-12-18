cdba482ba4b90e6ef430ee1a4e82a25b78fa348b
Ljubisa <ljubisa.rs@gmail.com>
2023-12-18 09:46:26 +0100

    chore: update readme.md file and bump version to v0.14.3

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package beekeeper
 
 var (
-	version = "0.14.0" // manually set semantic version number
+	version = "0.14.3" // manually set semantic version number
 	commit  string     // automatically set git commit hash
 
 	// Version TODO
