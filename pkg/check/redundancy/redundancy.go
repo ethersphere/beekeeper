e7583e93b44d35c29ed6f176b35d6611f431a3b5
Acha Bill <achabill12@gmail.com>
2024-02-27 17:36:59 +0100

    fix: wip

--- pkg/check/redundancy/redundancy.go
+++ pkg/check/redundancy/redundancy.go
@@ -142,7 +142,6 @@ func (c *Check) uploadChunks(ctx context.Context, client *bee.Client, chunks []s
 
 	c.logger.Infof("uploading %d chunks out of %d", len(indices), len(chunks))
 	for i, j := range indices {
-		c.logger.Infof("uploading chunk %d of %d", i+1, len(indices))
 		_, err := client.UploadChunk(ctx, chunks[j].Data(), api.UploadOptions{
 			BatchID: batchID,
 			Direct:  true,
