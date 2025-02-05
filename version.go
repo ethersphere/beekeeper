adc4496b94084720ff211497b4f55d53c1fd026f
Ljubisa Gacevic <ljubisa.rs@gmail.com>
2025-02-05 10:54:33 +0100

    chore: bump version to 0.26.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package beekeeper
 
 var (
-	version = "0.25.0" // manually set semantic version number
+	version = "0.26.0" // manually set semantic version number
 
 	commit string // automatically set git commit hash
 
