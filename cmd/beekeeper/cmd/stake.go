6f85296c503e0c46a476040e4a6045ac891dfdca
akrem-chabchoub <chabchoub.akrem@gmail.com>
2025-09-01 18:21:03 +0200

    feat(cmd): setup init stake cmd

---
+++ cmd/beekeeper/cmd/stake.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+    "github.com/spf13/cobra"
+)
+
+func (c *command) initStakeCmd() (err error) {
+    cmd := &cobra.Command{
+        Use:   "stake",
+        Short: "Stakes Bee nodes",
+        Long:  `Stakes Bee nodes with BZZ tokens and ETH for Bee node operations.`,
+        RunE: func(cmd *cobra.Command, args []string) (err error) {
+            return nil
+        },
+        PreRunE: c.preRunE,
+    }
+
+    c.root.AddCommand(cmd)
+
+    return nil
+}
+
