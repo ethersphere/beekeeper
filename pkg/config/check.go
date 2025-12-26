4c5f4a66a9c02ad32005b2281cd5b4db78a4dd78
mfw78 <mfw78@nxm.rs>
2025-12-26 14:51:27 +0000

    fix: remove unused kademlia check

--- pkg/config/check.go
+++ pkg/config/check.go
@@ -16,7 +16,6 @@ import (
 	"github.com/ethersphere/beekeeper/pkg/check/fullconnectivity"
 	"github.com/ethersphere/beekeeper/pkg/check/gc"
 	"github.com/ethersphere/beekeeper/pkg/check/gsoc"
-	"github.com/ethersphere/beekeeper/pkg/check/kademlia"
 	"github.com/ethersphere/beekeeper/pkg/check/load"
 	"github.com/ethersphere/beekeeper/pkg/check/longavailability"
 	"github.com/ethersphere/beekeeper/pkg/check/manifest"
@@ -187,24 +186,6 @@ var Checks = map[string]CheckType{
 				return nil, fmt.Errorf("decoding check %s options: %w", check.Type, err)
 			}
 			opts := gc.NewDefaultOptions()
-
-			if err := applyCheckConfig(checkGlobalConfig, checkOpts, &opts); err != nil {
-				return nil, fmt.Errorf("applying options: %w", err)
-			}
-
-			return opts, nil
-		},
-	},
-	"kademlia": {
-		NewAction: kademlia.NewCheck,
-		NewOptions: func(checkGlobalConfig CheckGlobalConfig, check Check) (any, error) {
-			checkOpts := new(struct {
-				Dynamic *bool `yaml:"dynamic"`
-			})
-			if err := check.Options.Decode(checkOpts); err != nil {
-				return nil, fmt.Errorf("decoding check %s options: %w", check.Type, err)
-			}
-			opts := kademlia.NewDefaultOptions()
 
 			if err := applyCheckConfig(checkGlobalConfig, checkOpts, &opts); err != nil {
 				return nil, fmt.Errorf("applying options: %w", err)
